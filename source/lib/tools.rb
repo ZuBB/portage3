8bc185f2b9e8a9b1b08a493b29954ac967092e09
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-01-12 00:48:07 +0200

    Greate bunch of cleanups for ebuild`s stuff

--- source/lib/tools.rb
+++ source/lib/tools.rb
@@ -48,6 +48,32 @@ def get_clean_value(line)
     return value
 end
 
+def get_ebuild_from_cvs_header(ebuild_text)
+    ebuild_text.each { |line|
+        if line.index('# $Header:') == 0
+            # https://bugs.gentoo.org/show_bug.cgi?id=398567
+            return line.match(/^# \$Header: ([^,]+),/)[1].to_s rescue 'package X'
+        end
+    }
+
+    return 'package X'
+end
+
+def get_single_line_ini_value(ebuild_text, keyword)
+    values = []
+    ebuild_text.each { |line|
+        # '==' because of app-editors/nvi/nvi-1.81.6-r3.ebuild
+        values << get_clean_value(line) if line.index(keyword) == 0
+    }
+
+    if (values.compact!.uniq! rescue []).size > 1
+        print "found #{values.size} values of '#{keyword}'"
+        puts "for #{get_ebuild_from_cvs_header(ebuild_text)}"
+    end
+
+    return values[0] rescue nil
+end
+
 def get_last_created_database(root_path, home_folder)
     # get last test database
     return Dir.glob(File.join(root_path, home_folder) + '/*.sqlite').sort.last
