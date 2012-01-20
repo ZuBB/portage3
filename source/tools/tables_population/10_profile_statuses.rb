aeb0ecd57b4871a537bef343dabcda1dcf50cf1c
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-01-20 15:16:39 +0200

    New table (profiles_statues)

---
+++ source/tools/tables_population/10_profile_statuses.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 01/11/12
+# Latest Modification: Vasyl Zuzyak, 01/11/12
+#
+$:.push File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+require 'optparse'
+require 'rubygems'
+require 'sqlite3'
+require 'tools'
+
+# hash with options
+options = Hash.new.merge!(OPTIONS)
+
+OptionParser.new do |opts|
+    # help header
+    opts.banner = " Usage: purge_s3_data [options]\n"
+    opts.separator " A script that purges outdated data from s3 bucket\n"
+
+    opts.on("-f", "--database-file STRING",
+            "Path where new database file will be created") do |value|
+        # TODO check if path id valid
+        options[:db_filename] = value
+    end
+
+    #TODO do we need a setting `:root` option here?
+    # parsing 'quite' option if present
+    opts.on("-q", "--quiet", "Quiet mode") do |value|
+        options[:quiet] = true
+    end
+
+    # parsing 'help' option if present
+    opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+    end
+end.parse!
+
+# get true portage home
+portage_home = get_full_tree_path(options)
+if options[:db_filename].nil?
+    # get last created database
+    options[:db_filename] = get_last_created_database(options)
+end
+
+def fill_table(params)
+    queries_array = results = []
+    sql_query = "INSERT INTO profile_statuses (profile_status) VALUES ('?');"
+    path = "#{params[:portage_home]}/profiles/profiles.desc"
+    return if !File.readable?(path)
+    # lets find all lines from profiles.desc file
+    results = %x[grep -oP '\t[a-z]*$' #{path}].to_a
+    # drop first item since its a header
+    results.shift()
+
+    results.uniq.compact.each { |status|
+        queries_array << sql_query.sub('?', status.strip!());
+    }
+
+    params[:database].execute_batch(queries_array.join("\n"))
+end
+
+fill_table_X(
+    options[:db_filename],
+    method(:fill_table),
+    {:portage_home => portage_home}
+)
+
