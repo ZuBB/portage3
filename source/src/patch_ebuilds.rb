2933aabb68a85cc887dc42ab8533f15024cde56c
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-02-05 11:21:05 +0200

    Added new patches

--- source/src/patch_ebuilds.rb
+++ source/src/patch_ebuilds.rb
@@ -17,11 +17,11 @@ options = Hash.new.merge!(OPTIONS)
 portage_home = get_full_tree_path(options)
 FileUtils.cd(portage_home)
 
-#`sed -i.bak -e '18d' ./dev-vcs/git/git-1.7.2.5.ebuild`
-#`sed -i.bak -e '22,28d' ./dev-vcs/git/git-1.7.2.5.ebuild`
+`sed -i.bak -e '18d' ./dev-vcs/git/git-1.7.2.5.ebuild`
+`sed -i.bak -e '22,28d' ./dev-vcs/git/git-1.7.2.5.ebuild`
 
-#`sed -i.bak -e '18d' ./dev-vcs/git/git-1.7.3.4-r1.ebuild`
-#`sed -i.bak -e '22,28d' ./dev-vcs/git/git-1.7.3.4-r1.ebuild`
+`sed -i.bak -e '18d' ./dev-vcs/git/git-1.7.3.4-r1.ebuild`
+`sed -i.bak -e '22,28d' ./dev-vcs/git/git-1.7.3.4-r1.ebuild`
 
 `sed -i.bak -e '182d' ./media-video/mplayer/mplayer-1.0_rc4_p20110322-r1.ebuild`
 `sed -i.bak -e '183,185d' ./media-video/mplayer/mplayer-1.0_rc4_p20110322-r1.ebuild`
@@ -40,3 +40,5 @@ FileUtils.cd(portage_home)
 
 `sed -i.bak -e '161,163d' ./media-video/mplayer/mplayer-9999.ebuild`
 `sed -i.bak -e '162d' ./media-video/mplayer/mplayer-9999.ebuild`
+
+`sed -i.bak 's/=/XXX/' ./games-strategy/openxcom/openxcom-9999.ebuild`
