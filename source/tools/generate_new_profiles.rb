2c13b8794d28ac82b1940e09f7772124b0d0c2c0
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-02-06 18:12:20 +0200

    update of profiles

--- source/tools/generate_new_profiles.rb
+++ source/tools/generate_new_profiles.rb
@@ -13,38 +13,57 @@ require 'tools'
 # hash with options
 options = Hash.new.merge!(OPTIONS)
 portage_home = get_full_tree_path(options)
-p portage_home
 FileUtils.cd(portage_home)
 
 # new profiles folder
 `mkdir -p profiles_v2`
+
 # content of the new profiles folder
 `cp profiles/* profiles_v2`
+
 # base folder
 `cp -r profiles/base profiles_v2/`
+
 # join 2 base package.mask files
 `cat profiles_v2/package.mask >> profiles_v2/base/package.mask`
 # make one of them 'missed'
 `mv profiles_v2/package.mask profiles_v2/package.mas_k`
+# remove uclibc
+line_num = `grep -nh '^sys-libs/uclibc' profiles_v2/base/package.mask | sed 's/:.*//'`
+`sed -i.bak -e '#{line_num.chomp!}d' profiles_v2/base/package.mask`
+
 # adding x86 arch
 `cp -r profiles/arch/x86 profiles_v2/base`
 # adding we do not care about xbox architecture for now
 `rm -r profiles_v2/base/x86/xbox`
+
 # adding amd64 arch
 `mkdir -p profiles_v2/base/amd64`
 # adding files for amd64 arch
 `cp profiles/arch/amd64/* profiles_v2/base/amd64`
+
 # adding linux platform for x86
 `mkdir -p profiles_v2/base/x86/linux`
 # adding linux platform for x86
 `cp profiles/default/linux/* profiles_v2/base/x86/linux`
+
 # adding linux platform for x86
 `mkdir -p profiles_v2/base/amd64/linux`
 # adding linux platform for amd64
 `cp profiles/default/linux/* profiles_v2/base/amd64/linux`
 
+# adding default feature
+`mkdir -p profiles_v2/base/x86/linux/default`
+# adding default feature
+`mkdir -p profiles_v2/base/amd64/linux/default`
+
+# adding files for default feature for x86
+`echo 'sys-libs/uclibc' > profiles_v2/base/x86/linux/default/package.mask`
+# adding files for default feature for amd64
+`echo 'sys-libs/uclibc' > profiles_v2/base/amd64/linux/default/package.mask`
+
 # adding subprofiles for x86
-`cp -r profiles/default/linux/x86/* profiles_v2/base/x86/linux/`
+`cp -r profiles/default/linux/x86/* profiles_v2/base/x86/linux/default/`
 # adding subprofiles for amd64
-`cp -r profiles/default/linux/amd64/* profiles_v2/base/amd64/linux/`
+`cp -r profiles/default/linux/amd64/* profiles_v2/base/amd64/linux/default/`
 
