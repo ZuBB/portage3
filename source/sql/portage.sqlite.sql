6e2fdefc54d3ef23ef4147c39fc7846cd3da46b8
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-02-07 23:39:18 +0200

    even more mysql compliance

--- source/sql/portage.sqlite.sql
+++ source/sql/portage.sqlite.sql
@@ -2,28 +2,28 @@ BEGIN TRANSACTION;
 
 create table system_settings (
     id INTEGER,
-    option VARCHAR NOT NULL UNIQUE,
+    option CHAR NOT NULL UNIQUE,
     value INTEGER NOT NULL,
     PRIMARY KEY (id)
 );
 
 create table architectures (
     id INTEGER,
-    architecture VARCHAR NOT NULL UNIQUE,
+    architecture CHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
     -- smth else?
 );
 
 create table platforms (
     id INTEGER,
-    platform_name VARCHAR NOT NULL UNIQUE,
+    platform_name CHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
 create table arches (
     -- here will be all items from profiles/arch.list file
     id INTEGER,
-    arch_name VARCHAR NOT NULL UNIQUE,
+    arch_name CHAR NOT NULL UNIQUE,
     architecture_id INTEGER NOT NULL,
     platform_id INTEGER NOT NULL,
     FOREIGN KEY (architecture_id) REFERENCES architectures(id),
@@ -34,15 +34,15 @@ create table arches (
 
 create table keywords (
     id INTEGER,
-    keyword VARCHAR NOT NULL UNIQUE,
-    -- symbol VARCHAR NOT NULL UNIQUE, /*really not null?*/
+    keyword CHAR NOT NULL UNIQUE,
+    -- symbol CHAR NOT NULL UNIQUE, /*really not null?*/
     PRIMARY KEY (id)
 );
 
 create table profiles (
     -- profile is any dir from 'base' basedir that has 'eapi' file inside
     id INTEGER,
-    profile_name VARCHAR NOT NULL,
+    profile_name CHAR NOT NULL,
     arch_id INTEGER NOT NULL,
     profile_status_id INTEGER NOT NULL,
     FOREIGN KEY (arch_id) REFERENCES arches(id),
@@ -53,13 +53,13 @@ create table profiles (
 
 create table profile_statuses (
     id INTEGER,
-    profile_status VARCHAR NOT NULL UNIQUE,
+    profile_status CHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
 create table sources (
     id INTEGER,
-    source VARCHAR NOT NULL UNIQUE,
+    source CHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
@@ -71,31 +71,31 @@ create table eapis (
 
 create table restriction_types (
     id INTEGER,
-    restriction VARCHAR NOT NULL UNIQUE,
-    -- sql_query VARCHAR NOT NULL UNIQUE,
+    restriction CHAR NOT NULL UNIQUE,
+    -- sql_query CHAR NOT NULL UNIQUE,
     -- CONSTRAINT idx1_unq UNIQUE (restriction, sql_query),
     PRIMARY KEY (id)
 );
 
 create table mask_states (
     id INTEGER,
-    mask_state VARCHAR NOT NULL UNIQUE,
+    mask_state CHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
 create table categories (
     id INTEGER,
-    category_name VARCHAR NOT NULL UNIQUE,
-    description VARCHAR NOT NULL,
+    category_name CHAR NOT NULL UNIQUE,
+    description CHAR NOT NULL,
     PRIMARY KEY (id)
 );
 
 create table packages (
     id INTEGER,
     category_id INTEGER NOT NULL,
-    package_name VARCHAR NOT NULL,
-    description VARCHAR NOT NULL,
-    homepage VARCHAR NOT NULL,
+    package_name CHAR NOT NULL,
+    description CHAR NOT NULL,
+    homepage CHAR NOT NULL,
     CONSTRAINT idx1_unq UNIQUE (category_id, package_name),
     FOREIGN KEY (category_id) REFERENCES categories(id),
     PRIMARY KEY (id)
@@ -103,14 +103,14 @@ create table packages (
 
 /* create table use_flags (
     id INTEGER,
-    flag_name VARCHAR NOT NULL UNIQUE,
-    flag_description VARCHAR NOT NULL UNIQUE,
+    flag_name CHAR NOT NULL UNIQUE,
+    flag_description CHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
 create table use_flags_states (
     id INTEGER,
-    flag_state VARCHAR NOT NULL UNIQUE,
+    flag_state CHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
@@ -161,16 +161,16 @@ create table users_use_flags2ebuilds (
 
 create table persons (
     id INTEGER,
-    name VARCHAR,
-    email VARCHAR NOT NULL UNIQUE,
-    nickname VARCHAR UNIQUE,
+    name CHAR,
+    email CHAR NOT NULL UNIQUE,
+    nickname CHAR UNIQUE,
     CONSTRAINT idx1_unq UNIQUE (name, email, nickname),
     PRIMARY KEY (id)
 );
 
 create table roles (
     id INTEGER,
-    role VARCHAR NOT NULL UNIQUE,
+    role CHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
@@ -197,12 +197,12 @@ create table person_roles2packages (
 create table ebuilds (
     id INTEGER,
     package_id INTEGER NOT NULL,
-    version VARCHAR NOT NULL,
-    license VARCHAR NOT NULL,
-    mtime VARCHAR NOT NULL,
-    mauthor VARCHAR NOT NULL,
+    version CHAR NOT NULL,
+    license CHAR NOT NULL,
+    mtime CHAR NOT NULL,
+    mauthor CHAR NOT NULL,
     eapi_id INTEGER NOT NULL,
-    slot VARCHAR NOT NULL,
+    slot CHAR NOT NULL,
     FOREIGN KEY (package_id) REFERENCES packages(id),
     FOREIGN KEY (eapi_id) REFERENCES eapis(id),
     CONSTRAINT idx1_unq UNIQUE (package_id, version),
@@ -232,7 +232,7 @@ create table package_keywords (
 create table package_masks (
     id INTEGER,
     package_id INTEGER NOT NULL,
-    version VARCHAR NOT NULL,
+    version CHAR NOT NULL,
     arch_id INTEGER NOT NULL,
     mask_state_id INTEGER NOT NULL,
     source_id INTEGER NOT NULL,
@@ -249,8 +249,8 @@ create table package_masks (
 
 /*create table licences (
     id INTEGER PRIMARY KEY,
-    license_name VARCHAR NOT NULL
-    -- url VARCHAR ?,
+    license_name CHAR NOT NULL
+    -- url CHAR ?,
     -- content blob zipped data
 );
 
@@ -268,7 +268,7 @@ create table licences2ebuilds (
 
 create table sets (
     id INTEGER PRIMARY KEY,
-    set_name VARCHAR NOT NULL
+    set_name CHAR NOT NULL
 );
 
 create table sets_content (
