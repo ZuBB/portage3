bf52d9784f0378d07618c2d7645f38bac7966e3f
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-03-23 10:04:18 +0200

    One more patch for ebuilds

--- source/tools/05_patch_ebuilds.rb
+++ source/tools/05_patch_ebuilds.rb
@@ -53,3 +53,6 @@ FileUtils.cd(portage_home)
 # https://bugs.gentoo.org/show_bug.cgi?id=409337
 `sed -i.bak1 -e '12,13d' ./sys-infiniband/libcxgb3/libcxgb3-1.2.5.ebuild`
 `sed -i.bak1 -e '13,14d' ./sys-infiniband/libehca/libehca-1.2.2.ebuild`
+
+# https://bugs.gentoo.org/show_bug.cgi?id=...
+`sed -i.bak1 's/nls php debug doc nls/nls php debug doc/' ./app-emulation/libguestfs/libguestfs-1.8.16.ebuild`
