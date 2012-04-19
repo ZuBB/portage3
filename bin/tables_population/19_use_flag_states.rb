d6f458673243799e35546d042f21c2942c439ff1
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-04-19 08:46:27 +0300

    Make this script work with current helper system

--- bin/tables_population/19_use_flag_states.rb
+++ bin/tables_population/19_use_flag_states.rb
@@ -3,65 +3,30 @@
 #
 # Here should go some comment
 #
-# Initial Author: Vasyl Zuzyak, 01/11/12
-# Latest Modification: Vasyl Zuzyak, 01/11/12
+# Initial Author: Vasyl Zuzyak, 01/20/12
+# Latest Modification: Vasyl Zuzyak, ...
 #
 $:.push File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
-require 'optparse'
-require 'rubygems'
-require 'sqlite3'
-require 'tools'
+require 'script'
 
-# hash with options
-options = Hash.new.merge!(OPTIONS)
 FLAG_STATES = ['disabled', 'enabled', 'masked', 'forced']
 
-OptionParser.new do |opts|
-    # help header
-    opts.banner = " Usage: purge_s3_data [options]\n"
-    opts.separator " A script that purges outdated data from s3 bucket\n"
-
-    opts.on("-f", "--database-file STRING",
-            "Path where new database file will be created") do |value|
-        # TODO check if path id valid
-        options[:db_filename] = value
-    end
-
-    #TODO do we need a setting `:root` option here?
-    # parsing 'quite' option if present
-    opts.on("-q", "--quiet", "Quiet mode") do |value|
-        options[:quiet] = true
-    end
-
-    # parsing 'help' option if present
-    opts.on_tail("-h", "--help", "Show this message") do
-        puts opts
-        exit
-    end
-end.parse!
-
-# get true portage home
-portage_home = get_full_tree_path(options)
-if options[:db_filename].nil?
-    # get last created database
-    options[:db_filename] = get_last_created_database(options)
-end
+script = Script.new({
+    "table" => "use_flags_states",
+    "script" => __FILE__
+})
 
 def fill_table(params)
     # array of all inserts
     queries_array = []
     # array of all keywords
     FLAG_STATES.each { |state|
-        # create query for keyword and add it into array
-        sql_query = "INSERT INTO use_flags_states (flag_state) VALUES ('#{state}');"
-        queries_array << sql_query
+        Database.insert({
+            "table" => params["table"],
+            "data" => {"flag_state" => state}
+        })
     }
-
-    params[:database].execute_batch(queries_array.join("\n"))
 end
 
-fill_table_X(
-    options[:db_filename],
-    method(:fill_table),
-    {}
-)
+script.fill_table_X(method(:fill_table))
+
