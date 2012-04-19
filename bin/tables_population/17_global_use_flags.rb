2cd78a77fab66a4dac81594cb308692c6066e597
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-04-19 22:32:06 +0300

    global flags

--- bin/tables_population/17_global_use_flags.rb
+++ bin/tables_population/17_global_use_flags.rb
@@ -11,37 +11,43 @@ require 'script'
 
 script = Script.new({
     "table" => "use_flags",
-    "script" => __FILE__
+    "script" => __FILE__,
+    "helper_query1" => "SELECT id FROM use_flags_types WHERE flag_type=?"
 })
 
 def fill_table(params)
-    # pattern for flag, its description (and package)
-    pattern = Regexp.new("([\\w\\/\\-]+:)?([\\w\\+\\-]+)(?: - )(.*)")
+    # pattern for flag, its description
+    pattern = Regexp.new("([\\w\\+\\-]+)(?: - )(.*)")
+    # flag type id
+    flag_type_id = Database.get_1value(params["helper_query1"], "global")
 
-    ["use.desc", "use.local.desc"].each do |file|
-        # get full filename
-        filename = File.join(params["portage_home"], "profiles_v2", file)
+    # read use flags and process each line
+    IO.foreach(File.join(params["portage_home"], "profiles_v2", "use.desc")) do |line|
+        # lets trim newlines
+        line.chomp!()
+        # skip comments or empty lines
+        next if line.index('#') == 0 or line.empty?
 
-        # read use flags and process each line
-        (IO.read(filename).to_a rescue []).each do |line|
-            # lets trim newlines
-            line.chomp!()
-            # skip comments or empty lines
-            next if line.index('#') == 0 or line.empty?
+        # lets get flag and desc
+        match = pattern.match(line)
 
-            # lets get flag and desc
-            match = pattern.match(line)
+        Database.insert({
+            "table" => params["table"],
+            "data" => {
+                "flag_name" => match[1],
+                "flag_description" => match[2],
+                "flag_type_id" => flag_type_id
+            }
+        })
+    end
 
-            Database.insert({
-                "table" => params["table"],
-                "command" => "INSERT OR REPLACE",
-                "data" => {
-                    "flag_name" => match[2],
-                    "flag_description" => match[3],
-                    "flag_type_id" => match[1].nil?() ? 1 : 2
-                }
-            })
-        end
+    sql_query = "SELECT COUNT(id) FROM #{params["table"]} WHERE flag_type_id=#{flag_type_id}"
+    total_global_flags = Database.db().get_first_value(sql_query)
+    sql_query = "SELECT COUNT(DISTINCT flag_name) FROM #{params["table"]} WHERE flag_type_id=#{flag_type_id}"
+    unique_global_flags = Database.db().get_first_value(sql_query)
+
+    if total_global_flags != unique_global_flags
+        PLogger.error("Its very likely that global flags have duplicates")
     end
 end
 
