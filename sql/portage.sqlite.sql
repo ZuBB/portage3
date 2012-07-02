c08f15c4e248f10fe48677694f76232341c8d25f
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-07-02 23:50:08 +0300

    Changes in SQL

--- sql/portage.sqlite.sql
+++ sql/portage.sqlite.sql
@@ -106,22 +106,6 @@ create table use_flags (
     PRIMARY KEY (id)
 );
 
-create table categories (
-    id INTEGER,
-    category_name VARCHAR NOT NULL UNIQUE,
-    description VARCHAR NOT NULL,
-    PRIMARY KEY (id)
-);
-
-create table packages (
-    id INTEGER,
-    category_id INTEGER NOT NULL,
-    package_name VARCHAR NOT NULL,
-    CONSTRAINT idx1_unq UNIQUE (category_id, package_name),
-    FOREIGN KEY (category_id) REFERENCES categories(id),
-    PRIMARY KEY (id)
-);
-
 /* create table persons (
     id INTEGER,
     name VARCHAR,
@@ -157,114 +141,96 @@ create table person_roles2packages (
     PRIMARY KEY (id)
 );*/
 
+create table repositories (
+    id INTEGER,
+    repository_name VARCHAR NOT NULL UNIQUE,
+    parent_folder VARCHAR NOT NULL,
+    repository_folder VARCHAR,
+    PRIMARY KEY (id)
+);
+
+create table categories (
+    id INTEGER,
+    category_name VARCHAR NOT NULL UNIQUE,
+    description VARCHAR NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create table packages (
+    id INTEGER,
+    category_id INTEGER NOT NULL,
+    package_name VARCHAR NOT NULL,
+    CONSTRAINT idx1_unq UNIQUE (category_id, package_name),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    PRIMARY KEY (id)
+);
+
 create table ebuilds (
     id INTEGER,
     package_id INTEGER NOT NULL,
     version VARCHAR NOT NULL,
-    version_order INTEGER NOT NULL,
-    mtime VARCHAR NOT NULL,
-    mauthor VARCHAR NOT NULL,
-    eapi_id INTEGER NOT NULL,
-    slot VARCHAR NOT NULL,
-    --eix_version_order INTEGER,
-    --portage_version_order INTEGER,
+    version_order INTEGER NOT NULL DEFAULT 0,
+    mtime INTEGER /*NOT NULL*/,
+    mauthor VARCHAR /*NOT NULL*/,
+    eapi_id INTEGER /*NOT NULL*/,
+    slot VARCHAR /*NOT NULL*/,
+    repository_id INTEGER NOT NULL,
+    description_id INTEGER NOT NULL DEFAULT 0,
+    homepage_id INTEGER NOT NULL DEFAULT 0,
     FOREIGN KEY (package_id) REFERENCES packages(id),
     FOREIGN KEY (eapi_id) REFERENCES eapis(id),
-    CONSTRAINT idx1_unq UNIQUE (package_id, version),
+    FOREIGN KEY (repository_id) REFERENCES repositories (id),
+    FOREIGN KEY (description_id) REFERENCES ebuild_descriptions (id),
+    FOREIGN KEY (homepage_id) REFERENCES ebuild_homepages (id),
+    CONSTRAINT idx1_unq UNIQUE (package_id, version, repository_id),
     PRIMARY KEY (id)
-    -- overlay/inherit/manifest
+    -- inherit/manifest
     -- data blob /*NOT NULL*/,
 );
 
-CREATE INDEX ebuilds_idx2 on ebuilds (package_id);
+CREATE INDEX ebuilds_idx2 on ebuilds (package_id, version);
+CREATE INDEX ebuilds_idx3 on ebuilds (package_id);
 
-create table package_descriptions (
+create table ebuild_descriptions (
     id INTEGER,
     description VARCHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
-create table descriptions2ebuilds (
-    id INTEGER,
-    description_id INTEGER NOT NULL,
-    package_id INTEGER NOT NULL,
-    ebuild_id INTEGER NOT NULL,
-    CONSTRAINT idx1_unq UNIQUE (description_id, package_id, ebuild_id),
-    FOREIGN KEY (description_id) REFERENCES package_descriptions(id),
-    FOREIGN KEY (package_id) REFERENCES packages(id),
-    FOREIGN KEY (ebuild_id) REFERENCES ebuilds(id),
-    PRIMARY KEY (id)
-);
-
-create table package_homepages (
+create table ebuild_homepages (
     id INTEGER,
     homepage VARCHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
-create table homepages2ebuilds (
+create table ebuild_keywords (
     id INTEGER,
-    homepage_id INTEGER NOT NULL,
-    package_id INTEGER NOT NULL,
-    ebuild_id INTEGER NOT NULL,
-    CONSTRAINT idx1_unq UNIQUE (homepage_id, package_id, ebuild_id),
-    FOREIGN KEY (homepage_id) REFERENCES package_homepages(id),
-    FOREIGN KEY (package_id) REFERENCES packages(id),
-    FOREIGN KEY (ebuild_id) REFERENCES ebuilds(id),
-    PRIMARY KEY (id)
-);
-
-create table package_licenses (
-    id INTEGER,
-    license VARCHAR NOT NULL UNIQUE,
-    -- url/hdd location ?
-    PRIMARY KEY (id)
-);
-
-create table licenses2ebuilds (
-    id INTEGER,
-    license_id INTEGER NOT NULL,
-    package_id INTEGER NOT NULL,
-    ebuild_id INTEGER NOT NULL,
-    CONSTRAINT idx1_unq UNIQUE (license_id, package_id, ebuild_id),
-    FOREIGN KEY (license_id) REFERENCES package_licenses(id),
-    FOREIGN KEY (package_id) REFERENCES packages(id),
-    FOREIGN KEY (ebuild_id) REFERENCES ebuilds(id),
-    PRIMARY KEY (id)
-);
-
-create table package_keywords (
-    id INTEGER,
-    package_id INTEGER NOT NULL,
     ebuild_id INTEGER NOT NULL,
     arch_id INTEGER NOT NULL,
     keyword_id INTEGER NOT NULL,
     source_id INTEGER NOT NULL,
-    FOREIGN KEY (package_id) REFERENCES packages(id),
     FOREIGN KEY (ebuild_id) REFERENCES ebuilds(id),
     FOREIGN KEY (arch_id) REFERENCES arches(id),
     FOREIGN KEY (keyword_id) REFERENCES keywords(id),
     FOREIGN KEY (source_id) REFERENCES sources(id),
     CONSTRAINT idx1_unq UNIQUE (
-        package_id, ebuild_id, arch_id, keyword_id, source_id
+        ebuild_id, arch_id, keyword_id, source_id
     ),
     PRIMARY KEY (id)
 );
 
-create table package_masks (
+create table ebuild_masks (
     id INTEGER,
-    package_id INTEGER NOT NULL,
-    ebuild_id VARCHAR NOT NULL,
+    ebuild_id INTEGER NOT NULL,
     arch_id INTEGER NOT NULL,
     mask_state_id INTEGER NOT NULL,
     source_id INTEGER NOT NULL,
-    FOREIGN KEY (package_id) REFERENCES packages(id),
     FOREIGN KEY (ebuild_id) REFERENCES ebuilds(id),
     FOREIGN KEY (arch_id) REFERENCES arches(id),
     FOREIGN KEY (mask_state_id) REFERENCES mask_states(id),
     FOREIGN KEY (source_id) REFERENCES sources(id),
     CONSTRAINT idx1_unq UNIQUE (
-        package_id, ebuild_id, arch_id, mask_state_id, source_id
+        ebuild_id, arch_id, mask_state_id, source_id
     ),
     PRIMARY KEY (id)
 );
@@ -283,6 +249,25 @@ create table use_flags2ebuilds (
     CONSTRAINT idx1_unq UNIQUE (ebuild_id, use_flag_id, flag_state),
     PRIMARY KEY (id)
 );
+
+/*create table package_licenses (
+    id INTEGER,
+    license VARCHAR NOT NULL UNIQUE,
+    -- url/hdd location ?
+    PRIMARY KEY (id)
+);
+
+create table licenses2ebuilds (
+    id INTEGER,
+    license_id INTEGER NOT NULL,
+    package_id INTEGER NOT NULL,
+    ebuild_id INTEGER NOT NULL,
+    CONSTRAINT idx1_unq UNIQUE (license_id, package_id, ebuild_id),
+    FOREIGN KEY (license_id) REFERENCES package_licenses(id),
+    FOREIGN KEY (package_id) REFERENCES packages(id),
+    FOREIGN KEY (ebuild_id) REFERENCES ebuilds(id),
+    PRIMARY KEY (id)
+);*/
 
 /*create table sets (
     id INTEGER PRIMARY KEY,
