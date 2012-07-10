5ad338089df4d967a05e982610151280aab359e8
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-07-10 18:46:54 +0300

    fill ebuilds's licences

--- bin/tables_population/37_ebuild_licences.rb
+++ bin/tables_population/37_ebuild_licences.rb
@@ -6,64 +6,57 @@
 # Initial Author: Vasyl Zuzyak, 04/20/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
-$:.push File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+lib_path_items = [File.dirname(__FILE__), '..', '..', 'lib']
+$:.push File.expand_path(File.join(*(lib_path_items + ['common'])))
+$:.push File.expand_path(File.join(*(lib_path_items + ['portage'])))
 require 'script'
 require 'ebuild'
 
+def get_data(params)
+    # results
+    results = []
+    # query
+    sql_query = <<-SQL
+        SELECT
+            parent_folder,
+            repository_folder,
+            category_name,
+            package_name,
+            version
+        FROM ebuilds e
+        JOIN packages p on p.id=e.package_id
+        JOIN categories c on p.category_id=c.id
+        JOIN repositories r on r.id=e.repository_id
+    SQL
+
+    # lets walk through all packages
+    Database.select(sql_query).each { |row|
+        results << {
+            'value' => row[3] + '-' + row[4] + '.ebuild',
+            'parent_dir' => File.join(row[0], row[1], row[2], row[3])
+        }
+    }
+
+    return results
+end
+
+def process(params)
+    PLogger.info("Ebuild: #{params["value"]}")
+    ebuild = Ebuild.new(params)
+
+    ebuild.ebuild_licences().split(' ').each { |licence|
+        Database.add_data4insert([ebuild.ebuild_id(), licence])
+    }
+end
+
 script = Script.new({
     "script" => __FILE__,
-    "table" => "package_licenses",
-    "helper_query1" => <<SQL
-INSERT INTO licenses2ebuilds
-(license_id, package_id, ebuild_id)
-VALUES (?, ?, ?)
-SQL
+    "thread_code" => method(:process),
+    "data_source" => method(:get_data),
+    "sql_query" => <<-SQL
+        INSERT INTO ebuild_licences
+        (ebuild_id, licence_id)
+        VALUES (?, (SELECT id FROM licences WHERE name=?));
+    SQL
 })
-
-def parse_ebuild(params)
-    PLogger.info("Ebuild: #{params["filename"]}")
-    Ebuild.new(Utils.create_ebuild_params(params))
-end
-
-def store_ebuild_licenses(ebuild, tablename)
-    license_ids = []
-    ebuild.license().split(' ').each { |license|
-        license_ids << Database.insert({
-            "table" => tablename,
-            "command" => "INSERT OR REPLACE",
-            "data" => {"license" => license}
-        })
-    }
-
-    return license_ids
-end
-
-def link_licenses2ebuild(ebuild, license_ids, sql_query)
-    license_ids.each { |license_id|
-        Database.insert({
-            "sql_query" => sql_query,
-            "values" => [license_id, ebuild.package_id(), ebuild.ebuild_id()]
-        })
-    }
-end
-
-def category_block(params)
-    Utils.walk_through_packages({"block2" => method(:packages_block)}.merge!(params))
-end
-
-def packages_block(params)
-    Dir.glob(File.join(params["item_path"], '*.ebuild')).each do |ebuild|
-        ebuild = parse_ebuild({"filename" => ebuild}.merge!(params))
-        license_ids = store_ebuild_licenses(ebuild, params["table"])
-        link_licenses2ebuild(ebuild, license_ids, params["helper_query1"])
-    end
-end
-
-def fill_table(params)
-    Utils.walk_through_categories(
-        {"block1" => method(:category_block)}.merge!(params)
-    )
-end
-
-script.fill_table_X(method(:fill_table))
 
