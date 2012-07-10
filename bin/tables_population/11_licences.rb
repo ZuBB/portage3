49b7bac7c31694fe45a02ba23fb7325db3ace4a8
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-07-10 18:47:49 +0300

    fill licences

---
+++ bin/tables_population/11_licences.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 01/11/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+lib_path_items = [File.dirname(__FILE__), '..', '..', 'lib']
+$:.push File.expand_path(File.join(*(lib_path_items + ['common'])))
+require 'script'
+
+def get_data(params)
+    # get all files from licenses dir
+    (Dir[File.join(params["gentoo_tree_home"], 'licenses/*')].map { |item|
+        File.file?(item) ?
+            item.slice((params["gentoo_tree_home"] + 'licenses/').size + 1..-1) : nil
+    }).compact
+end
+
+def process(params)
+    Database.add_data4insert([params['value']])
+end
+
+script = Script.new({
+    'script' => __FILE__,
+    'thread_code' => method(:process),
+    'sql_query' => 'INSERT INTO licences (name) VALUES (?);',
+    'data_source' => method(:get_data),
+})
+
