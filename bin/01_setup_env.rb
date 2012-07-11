ce19a5c810e8ad68243d90c85a46c9a6f944e4d2
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-07-11 19:18:31 +0300

    Rename of other files in bin dir

---
+++ bin/01_setup_env.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 01/04/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+puts 'TODO: run all stuff from `env_setup` folder'
+
