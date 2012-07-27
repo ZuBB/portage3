ce00ab26f3072c0248a911d67c260d204029c223
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-07-27 16:41:21 +0300

    Added separate function that does version sorting

--- lib/portage/package.rb
+++ lib/portage/package.rb
@@ -16,6 +16,9 @@ class Package < Category
         'all@c_id' => 'select * from packages where category_id=?',
         'id' => 'SELECT id FROM packages WHERE package_name=? and category_id = ?'
     }
+    VER_REGEXP = Regexp.new("^(cvs\\.)?(\\d+)((\\.\\d+)*)([a-z]?)((_(pre|p|beta|alpha|rc)\\d*)*)(-r(\\d+))?$")
+    SUFFIX_REGEXP = Regexp.new("^(alpha|beta|rc|pre|p)(\\d*)$")
+    SUFFIX_VALUE = {"pre"=> -2, "p"=> 0, "alpha"=> -4, "beta"=> -3, "rc"=> -1}
 
     def initialize(params)
         super(params)
@@ -55,40 +58,155 @@ class Package < Category
         @package_ldescription = ldescription
     end
 
-    def self.get_package_versions(atom)
-        if Utils::SETTINGS['eix_presence']
-            self.get_eix_versions(atom)
-        else
-            self.get_portage_versions(atom)
+	# Ruby version of vercmp function from
+	# /usr/lib/portage/pym/portage/versions.py
+    def self.vercmp(ver1, ver2)
+        if ver1 == ver2
+            return 0
         end
-    end
 
-    def self.get_eix_versions(atom)
-        versions_line = %x[eix -x --end #{atom} | grep 'Available versions']
+        match1 = ver1.match(VER_REGEXP)
+        match2 = ver2.match(VER_REGEXP)
 
-        # drop wording at start
-        versions_line.sub!('Available versions:', '')
+        # checking that the versions are valid
+        return nil unless match1
+        return nil unless match2
 
-        # drop use flags
-        versions_line.sub!(/\{[^\}]+\}\s*$/, "")
+        # shortcut for cvs ebuilds (new style)
+        if match1[1] && !match2[1]
+            return 1
+        elsif match2[1] && !match1[1]
+            return -1
+        end
 
-        # get versions and make it looks nice
-        versions_line.strip().split(' ').map! { |version|
-            version.sub!(/!.+/, '')
-            version.gsub!(/\([^\)]+\)/, '')
-            version.gsub!(/\[[^\]]+\]/, '')
-            version.sub!(/\+[iv]$/, '')
-            version.sub!(/[~*]+/, '')
-            version
-        }
-    end
+        # building lists of the version parts before the suffix
+        # first part is simple
+        list1 = [match1[2].to_i]
+        list2 = [match2[2].to_i]
 
-    def self.get_portage_versions(atom)
-        tool_path = '/../../bin/list_package_ebuilds.py'
-        command = File.dirname(__FILE__) + tool_path + ' ' + atom
-        %x[#{command}].split("\n").map { |line|
-            line[atom.size + 1..-1]
-        }
+        # this part would greatly benefit from a fixed-length version pattern
+        if match1[3] || match2[3]
+            vlist1 = match1[3].to_s[1..-1].split(".") rescue ''
+            vlist2 = match2[3].to_s[1..-1].split(".") rescue ''
+
+            for i in 0...[vlist1.size, vlist2.size].max
+                # Implcit .0 is given a value of -1, so that 1.0.0 > 1.0, since it
+                # would be ambiguous if two versions that aren't literally equal
+                # are given the same value (in sorting, for example).
+                if vlist1.size <= i || vlist1[i].size == 0
+                    list1.push(-1)
+                    list2.push(vlist2[i].to_i)
+                elsif vlist2.size <= i || vlist2[i].size == 0
+                    list1.push(vlist1[i].to_i)
+                    list2.push(-1)
+                    # Let's make life easy and use integers unless we're forced to use floats
+                elsif (vlist1[i][0] != "0" && vlist2[i][0] != "0")
+                    list1.push(vlist1[i].to_i)
+                    list2.push(vlist2[i].to_i)
+                    # now we have to use floats so 1.02 compares correctly against 1.1
+                else
+                    # list1.append(float("0."+vlist1[i]))
+                    # list2.append(float("0."+vlist2[i]))
+                    # Since python floats have limited range, we multiply both
+                    # floating point representations by a constant so that they are
+                    # transformed into whole numbers. This allows the practically
+                    # infinite range of a python int to be exploited. The
+                    # multiplication is done by padding both literal strings with
+                    # zeros as necessary to ensure equal length.
+                    max_len = [vlist1[i].size, vlist2[i].size].max
+                    list1.push(vlist1[i].ljust(max_len, "0").to_i)
+                    list2.push(vlist2[i].ljust(max_len, "0").to_i)
+                end
+            end
+        end
+
+        # and now the final letter
+        # NOTE: Behavior changed in r2309 (between portage-2.0.x and portage-2.1).
+        # The new behavior is 12.2.5 > 12.2b which, depending on how you look at,
+        # may seem counter-intuitive. However, if you really think about it, it
+        # seems like it's probably safe to assume that this is the behavior that
+        # is intended by anyone who would use versions such as these.
+        if match1[5].size > 0
+            list1.push(match1[5].ord)
+        end
+
+        if match2[5].size > 0
+            list2.push(match2[5].ord)
+        end
+
+        for i in 0...[list1.size, list2.size].max
+            if list1.size <= i
+                return -1
+            elsif list2.size <= i
+                return 1
+            elsif list1[i] != list2[i]
+                a = list1[i]
+                b = list2[i]
+                return a - b
+            end
+        end
+
+        # main version is equal, so now compare the _suffix part
+        list1 = match1[6].split("_")[1..-1]
+        list2 = match2[6].split("_")[1..-1]
+
+        for i in 0...[list1.to_s.size, list2.to_s.size].max
+            # Implicit _p0 is given a value of -1, so that 1 < 1_p0
+            if list1.nil? || list1[i].nil?
+                s1 = ["p","-1"]
+            else
+                s1 = SUFFIX_REGEXP.match(list1[i]).to_a.drop(1)
+            end
+
+            if list2.nil? || list2[i].nil?
+                s2 = ["p","-1"]
+            else
+                s2 = SUFFIX_REGEXP.match(list2[i]).to_a.drop(1)
+            end
+
+            if s1[0] != s2[0]
+                a = SUFFIX_VALUE[s1[0]]
+                b = SUFFIX_VALUE[s2[0]]
+                rval = a - b
+                return rval
+            end
+
+            if s1[1] != s2[1]
+                # it's possible that the s(1|2)[1] == ''
+                # in such a case, fudge it.
+                begin
+                    r1 = s1[1].to_i
+                rescue
+                    r1 = 0
+                end
+
+                begin
+                    r2 = s2[1].to_i
+                rescue
+                    r2 = 0
+                end
+
+                if (rval = (r1 - r2))
+                    return rval
+                end
+            end
+        end
+
+        # the suffix part is equal to, so finally check the revision
+        if !match1[10].nil? && match1[10].size > 0
+            r1 = match1[10].to_i
+        else
+            r1 = 0
+        end
+
+        if !match2[10].nil? && match2[10].size > 0
+            r2 = match2[10].to_i
+        else
+            r2 = 0
+        end
+
+        rval = (r1 - r2)
+        return rval
     end
 
     def self.get_packages(params = {})
