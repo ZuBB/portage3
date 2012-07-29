8a4412ef22b3ea40858f487e5a6a0e422315f966
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-07-30 01:09:24 +0300

    Added support of automatic loading of check modules

--- bin/db_population/envsetup.rb
+++ bin/db_population/envsetup.rb
@@ -8,24 +8,28 @@
 Encoding.default_external = 'UTF-8'
 Encoding.default_internal = 'UTF-8'
 module EnvSetup
-    def self.get_path2root
-        '../..'
+    PATH2ROOT = '../..'
+    BASEDIR = File.dirname(__FILE__)
+
+    def self.get_path2lib(basedir = BASEDIR)
+        File.expand_path(File.join(basedir, PATH2ROOT, 'lib'))
     end
 
-    def self.get_path2lib(basedir = File.dirname(__FILE__))
-        path_parts = [basedir, self.get_path2root, 'lib']
-        File.expand_path(File.join(*path_parts))
-    end
-
-    def self.get_lib_dirs()
+    def self.get_lib_dirs
         Dir[File.join(self.get_path2lib, '*')].delete_if { |item| 
             File.file?(item)
         }
     end
 end
 
-EnvSetup.get_lib_dirs().each { |libdir|
+EnvSetup.get_lib_dirs.each { |libdir|
     $:.push File.expand_path(libdir)
 }
 
+check_filename = $0.dup.insert($0.rindex('.'), '.check')
+check_filepath = File.join(EnvSetup::BASEDIR, check_filename)
+check_module_name = File.basename(check_filename, ".rb")
+
 require 'script'
+require_relative check_module_name if File.exist?(check_filepath)
+
