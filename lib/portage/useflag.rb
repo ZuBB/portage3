002554767032d298211e19429158a52e758842c5
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-07-31 11:59:02 +0300

    Useflag lib extended

--- lib/portage/useflag.rb
+++ lib/portage/useflag.rb
@@ -4,21 +4,32 @@
 # Initial Author: Vasyl Zuzyak, 04/02/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
-require 'dbobject'
-
 class UseFlag
-    include DBobject
-
     ENTITY = self.name.downcase[0..-7]
-    PROP_SUFFIXES = ['name', 'description', 'type_id']
-    TYPES = ['global', 'local', 'expand', 'hidden']
-    SQL = { 'type' => 'SELECT id FROM flag_types WHERE type=?' }
+    TYPES = ['hidden', 'expand', 'local', 'global']
     STATES = ['masked', 'disabled', 'enabled', 'forced']
-    Regexps = {
+    SQL = { 'type' => 'SELECT id FROM flag_types WHERE type=?' }
+    REGEXPS = {
         'local'  => Regexp.new("([\\w\\/\\-\\+]+:)?([\\w\\+\\-]+)(?:\\s-\\s)(.*)"),
         'expand' => Regexp.new('([\\w\\+\\-]+)(?:\\s-\\s)(.*)'),
         'hidden' => Regexp.new('([\\w\\+\\-]+)(?:\\s-\\s)(.*)'),
-        'global' => Regexp.new('([\\w\\+\\-]+)(?:\\s-\\s)(.*)')
+        'global' => Regexp.new('([\\w\\+\\-]+)(?:\\s-\\s)(.*)'),
+        'state' => Regexp.new('^[^\\w]?')
     }
+
+    def self.get_flag(flag)
+        flag.sub(REGEXPS['state'], '')
+    end
+
+    def self.get_flag_state(flag)
+        # app-doc/pms section 8.2
+        sign = REGEXPS['state'].match(flag).to_s
+        case sign
+        when '-' then 'disabled'
+        when '+' then 'enabled'
+        when '' then 'enabled'
+        else nil
+        end
+    end
 end
 
