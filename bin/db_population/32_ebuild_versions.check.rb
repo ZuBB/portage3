93d63f2c2ab5ead3af6e89150a2ca4a129b619ae
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-08-10 11:05:12 +0300

    Moved common checks in versions module

--- bin/db_population/32_ebuild_versions.check.rb
+++ bin/db_population/32_ebuild_versions.check.rb
@@ -8,50 +8,7 @@
 #
 class Script
     def post_insert_check
-        EbuildVersion.post_insert_check([
-            {
-                'message' => 'Next packages have zeros in `version_order` column',
-                'sql_query' => <<-SQL
-                    select c.name, p.name, p.id
-                    from ebuilds e
-                    join packages p on e.package_id=p.id
-                    join categories c on p.category_id=c.id
-                    where version_order<1
-                    group by package_id;
-                SQL
-            },
-            {
-                'message' => 'Next packages have version_order > count(ebuild_id)',
-                'sql_query' => <<-SQL
-                    select c.name, p.name, p.id
-                    from (
-                        select
-                            package_id,
-                            count(package_id) as ebuilds,
-                            max(version_order) as max_version_order
-                        from ebuilds
-                        group by package_id
-                    ) e
-                    join packages p on e.package_id=p.id
-                    join categories c on p.category_id=c.id
-                    where ebuilds!=max_version_order;
-                SQL
-            },
-            {
-                'message' => 'Next packages have dups in `version_order` per same package_id',
-                'sql_query' => <<-SQL
-                    select c.name, p.name, p.id
-                    from (
-                        select distinct package_id, count(id) as counter
-                        from ebuilds
-                        group by package_id, version_order
-                        having counter > 1
-                    ) e
-                    join packages p on e.package_id=p.id
-                    join categories c on p.category_id=c.id;
-                SQL
-            }
-        ])
+        EbuildVersion.post_insert_check(MAIN_CHECK)
     end
 end
 
