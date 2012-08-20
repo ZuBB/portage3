6ac0b85741af7c82040a108fbaba702ad16b03da
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-08-20 08:53:54 +0300

    Changes in existing scripts required after previous commit

--- bin/db_population/13_categories.rb
+++ bin/db_population/13_categories.rb
@@ -10,18 +10,32 @@ require_relative 'envsetup'
 require 'category'
 
 class Script
+    SOURCE = 'profiles'
+
+    def pre_insert_task
+        sql_query = 'select id from sources where source=?;'
+        source_id = Database.get_1value(sql_query, SOURCE)
+        @shared_data['source@id'] = { SOURCE => source_id }
+    end
+
     def process(params)
         PLogger.debug("Category: #{params}")
         category = Category.new(params)
 
-        Database.add_data4insert(category.category,
-                                 category.category_description
-                                )
+        params = [category.category]
+        params << category.category_description
+        params << @shared_data['source@id'][SOURCE]
+
+        Database.add_data4insert(params)
     end
 end
 
 script = Script.new({
     'data_source' => Category.method(:get_categories),
-    'sql_query' => 'INSERT INTO categories (name, descr) VALUES (?, ?);'
+    'sql_query' => <<-SQL
+        INSERT INTO categories
+        (name, descr, source_id)
+        VALUES (?, ?, ?);
+    SQL
 })
 
--- bin/db_population/14_packages.rb
+++ bin/db_population/14_packages.rb
@@ -10,16 +10,32 @@ require_relative 'envsetup'
 require 'package'
 
 class Script
+    SOURCE = 'profiles'
+
+    def pre_insert_task
+        sql_query = 'select id from sources where source=?;'
+        source_id = Database.get_1value(sql_query, SOURCE)
+        @shared_data['source@id'] = { SOURCE => source_id }
+    end
+
     def process(params)
         PLogger.debug("Package: #{params}")
         package = Package.new(params)
 
-        Database.add_data4insert(package.category_id, package.package)
+        params = [package.package]
+        params << package.category_id
+        params << @shared_data['source@id'][SOURCE]
+
+        Database.add_data4insert(params)
     end
 end
 
 script = Script.new({
     'data_source' => Package.method(:get_packages),
-    'sql_query' => 'INSERT INTO packages (category_id, name) VALUES (?, ?);'
+    'sql_query' => <<-SQL
+        INSERT INTO packages
+        (name, category_id, source_id)
+        VALUES (?, ?, ?);
+    SQL
 })
 
