595a8c5091f64c5825798fbd10b0151018a80a57
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-08-20 08:54:52 +0300

    Added new method that handles '-*' flags

--- lib/portage/useflag.rb
+++ lib/portage/useflag.rb
@@ -50,5 +50,29 @@ class UseFlag
         else nil
         end
     end
+
+    def self.expand_asterix_flag(line, package_id)
+        sql_query = <<-SQL
+            select distinct f.name
+            from flags f
+            join flags_states fs on fs.flag_id = f.id
+            join ebuilds e on e.id = fs.ebuild_id
+            where e.package_id = ?;
+        SQL
+        all_flags = Database.select(sql_query, package_id).flatten
+
+        flags = line.split.drop(1)
+
+        asterix_index = flags.index { |i| i.include?('*') }
+        asterix_flag = flags.delete_at(asterix_index)
+        asterix_state = self.get_flag_state(asterix_flag)
+        asterix_state_sign = asterix_state == 'disabled' ? '-' : ''
+
+        flag_names = flags.map { |f| self.get_flag(f) }
+        new_flags = all_flags - flag_names
+        new_flags.map! { |f| f.insert(0, asterix_state_sign) }
+
+        flags.insert(asterix_index, new_flags.join(' ')).join(' ')
+    end
 end
 
