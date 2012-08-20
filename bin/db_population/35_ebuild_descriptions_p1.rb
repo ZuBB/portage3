2549cab5eb6ed619c9be0b9a434d289fc95c478c
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-08-20 18:53:13 +0300

    Moved logged message to anoter level

--- bin/db_population/35_ebuild_descriptions_p1.rb
+++ bin/db_population/35_ebuild_descriptions_p1.rb
@@ -10,7 +10,7 @@ require_relative 'envsetup'
 require 'ebuild'
 
 class Script
-    def pre_insert_task()
+    def pre_insert_task
         sql_query = <<-SQL
             DROP TABLE IF EXISTS tmp_ebuild_descriptions;
 
@@ -27,7 +27,7 @@ class Script
     end
 
     def process(params)
-        PLogger.info("Ebuild: #{params[3, 3].join('-')}")
+        PLogger.debug("Ebuild: #{params[3, 3].join('-')}")
         ebuild = Ebuild.new(Ebuild.generate_ebuild_params(params))
 
         Database.add_data4insert(ebuild.ebuild_description, ebuild.ebuild_id)
