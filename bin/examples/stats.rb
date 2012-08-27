7b131553289da9319a751c5cd1d935520130bbd7
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-08-27 16:13:13 +0300

    Some update of stats

--- bin/examples/stats.rb
+++ bin/examples/stats.rb
@@ -5,33 +5,124 @@
 # Initial Author: Vasyl Zuzyak, 01/04/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
-$:.push File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'common'))
-#require 'optparse'
-#require 'database'
+$:.push File.expand_path(File.join(File.dirname(__FILE__), '../lib/common'))
+require 'optparse'
+require 'sqlite3'
 #require 'utils'
 
-puts 'TODO: implementation missed :)'
+REPORTS = [
+    {
+        'descr' => 'Total categories',
+        'query' => 'select count(id) from categories;'
+    },
+    {
+        'descr' => 'Top 5 categories with max packages',
+        'query' => <<-SQL
+            select c.name, count(p.id) as packages_count
+            from packages p
+            join categories c on p.category_id=c.id
+            group by p.category_id
+            order by packages_count desc
+            limit 5;
+        SQL
+    },
+    {
+        'descr' => 'Top 5 categories with min packages',
+        'query' => <<-SQL
+            select c.name, count(p.id) as packages_count
+            from packages p
+            join categories c on p.category_id=c.id
+            group by p.category_id
+            order by packages_count asc
+            limit 5;
+        SQL
+    },
+    {
+        'descr' => 'Total packages',
+        'query' => 'select count(id) from packages;'
+    },
+    {
+        'descr' => 'Top packages by amount of ebuilds per package',
+        'query' => <<-SQL
+            select ebuilds_count as ebuilds_per_package, count(id) as packages
+            from (
+                SELECT p.id, COUNT(e.id) AS ebuilds_count
+                FROM ebuilds e
+                JOIN packages p ON e.package_id=p.id
+                GROUP BY p.package_id
+                ORDER BY ebuilds_count ASC
+            ) em
+            group by ebuilds_count
+            order by packages desc;
+        SQL
+    },
+    {
+        'descr' => 'Packages without ebuilds',
+        'query' => <<-SQL
+            select c.category_name, p.name
+            from packages p
+            join categories c on p.category_id=c.id
+            where p.id not in (select package_id from ebuilds)
+            order by c.category_name, p.name asc;
+        SQL
+    },
+    {
+        'descr' => 'Total ebuilds',
+        'query' => 'select count(id) from ebuilds;'
+    },
+    {
+        'descr' => 'Top 5 newest ebuilds',
+        'query' => <<-SQL
+            select c.name, p.name, e.version, e.mtime
+            from ebuilds e
+            join packages p on e.package_id=p.id
+            join categories c on p.category_id=c.id
+            order by e.mtime desc
+            limit 5;
+        SQL
+    },
+    {
+        'descr' => 'Top 5 oldest ebuilds',
+        'query' => <<-SQL
+            select c.name, p.name, e.version, e.mtime
+            from ebuilds e
+            join packages p on e.package_id=p.id
+            join categories c on p.category_id=c.id
+            order by e.mtime asc
+            limit 5;
+        SQL
+    },
+    {
+        'descr' => 'Top 5 authors that change max amount of ebuilds',
+        'query' => <<-SQL
+            select mauthor, count(e.id) as ebuilds_count
+            from ebuilds
+            group by mauthor.
+            order by ebuilds_count desc
+            limit 5;
+        SQL
+    },
+    {
+        'descr' => 'Top packages by amount of ebuilds per package',
+        'descr' => 'Top ebuilds by amount of ebuilds per package',
+        # top 5 authors that change min ebuilds
+        'query' => <<-SQL
+            select ebuilds_count as ebuilds_per_package, count(id) as packages
+            from (
+                SELECT p.id, COUNT(e.id) AS ebuilds_count
+                FROM ebuilds e
+                JOIN packages p ON e.package_id=p.id
+                GROUP BY p.package_id
+                ORDER BY ebuilds_count ASC
+            ) em
+            group by ebuilds_count
+            order by packages desc;
+        SQL
+    },
+]
 
-'.timer ON'
-R = [
-<<QUERIES
-
-select flag_name, count(*)
-from use_flags
-group by flag_name
-having count(*) > 1
-
-total categories
-top 5 categories with max packages
-top 5 categories with min packages
-total packages
-top 5 packages with max ebuilds
-top 5 packages with min ebuilds
-total ebuilds
-top 5 newest changed ebuilds
-top 5 oldest changed ebuilds
-top 5 authors that change max ebuilds
-top 5 authors that change min ebuilds
+DESCRIPTIONS = <<TXT
+total eapis
 total slots
 top 5 slots by max amount of ebuilds
 top 5 slots by min amount of ebuilds
@@ -59,10 +150,22 @@ top 5 widely used use_flags
 top 5 packages with max use_flags enabled
 top 5 packages with max use_flags disabled
 total licences
+licences that does not belong to any group
+licence groups that does not have any licence in it
 top 5 most used licences
 top 5 rare used licences
 top by amount of used licences 
 total records
-QUERIES
-]
+TXT
+
+#select flag_name, count(*)
+#from use_flags
+#group by flag_name
+#having count(*) > 1
+
+#test.each { |block|
+    #puts block['descr']
+    #res = database.execute2(block['query'])
+    #puts res.to_table(:first_row_is_head => true).to_s
+#}
 
