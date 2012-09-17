0296e562bc021b997ce0e5f2e4f5498c122916b3
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-09-17 14:24:14 +0300

    Removed useless parenteeths

--- bin/env_setup/02_prepare_fast_storage.rb
+++ bin/env_setup/02_prepare_fast_storage.rb
@@ -66,8 +66,8 @@ OptionParser.new do |opts|
     end
 end.parse!
 
-settings = Utils.get_settings()
-portage_home = Utils.get_tree_home()
+settings = Utils.get_settings
+portage_home = Utils.get_tree_home
 root_path = File.dirname(portage_home)
 
 unless File.exist?(root_path)
@@ -86,7 +86,7 @@ unless File.writable?(root_path)
 end
 
 space_available = `df -kP #{root_path}`.split("\n")[1].split(" ")[3].to_i
-portage_size = `du -s #{root_path}`.split()[0].to_i rescue 0
+portage_size = `du -s #{root_path}`.split[0].to_i rescue 0
 required_space = options["required_space"] * 1024
 
 if portage_size + space_available < required_space
