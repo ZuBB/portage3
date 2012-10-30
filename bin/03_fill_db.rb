27f61cea6afff7163414438d9e1b69fc9aef25d0
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-10-30 13:38:47 +0200

    Parallel database fill: filler script rework

--- bin/03_fill_db.rb
+++ bin/03_fill_db.rb
@@ -6,42 +6,62 @@
 # Initial Author: Vasyl Zuzyak, 6 01/06/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
-$:.push File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'common'))
+current_dir = File.dirname(__FILE__)
+$:.push File.expand_path(File.join(current_dir, '..', 'lib', 'common'))
+$:.push File.expand_path(File.join(current_dir, '..', 'lib', 'portage'))
+$:.push File.expand_path(File.join(current_dir, '..', 'tasks'))
+
 require 'optparse'
-require 'utils'
+require 'tasks'
+require 'scheduler'
 
 # hash with options
-options = Hash.new.merge!(Utils::OPTIONS)
-# get last created database
-options["db_filename"] = Utils.get_database()
+options = {
+    "from" => 1,
+    "until" => 999,
+    "skip" => [],
+    "task_filenames" => []
+}
+
+# get default options
+options.merge!(Utils::OPTIONS)
 
 OptionParser.new do |opts|
     # help header
     opts.banner = " Usage: 03_fill_db [options]\n"
-    opts.separator " A script that fills cache db with all data"
+    opts.separator " A script that fills cache db with data"
 
-    opts.on("-f", "--database-file STRING",
-            "Path to database file to fill") do |value|
+    # parsing 'threads' option if present
+    opts.on("-1", "--one-thread", "Use 1 thread for processing") do
+        options["threads"] = 1
+    end
+
+    opts.on("-f", "--database-file STRING", "Path to database file to fill") do |value|
         # TODO check if path id valid
         options["db_filename"] = value
     end
 
-    # parsing 'quite' option if present
-    opts.on("-q", "--quiet", "Quiet mode") do |value|
-        options["quiet"] = true
+    # parsing 'debug' option if present
+    opts.on("-d", "--debug", "Set log level to 'debug'") do |value|
+        options["debug"] = true
     end
 
-    # parsing 'quite' option if present
+    # parsing 'log-device' option if present
+    opts.on("--log-device STRING", "Your custom log device. Applies only if '-t' option is used") do |value|
+        options["log-device"] = value
+    end
+
+    # parsing 'method' option if present
     opts.on("-m", "--method STRING", "Parse method") do |value|
         options["method"] = value
     end
 
-    # parsing 'untill' option if present
+    # parsing 'from' option if present
     opts.on("-r", "--from STRING", "Start from specified script") do |value|
         options["from"] = value.to_i
     end
 
-    # parsing 'untill' option if present
+    # parsing 'skip' option if present
     opts.on("-s", "--skip STRING", "Skip specified scripts") do |value|
         options["skip"] = value.split(',').map { |i| i.strip.to_i }
     end
@@ -51,6 +71,18 @@ OptionParser.new do |opts|
         options["until"] = value.to_i
     end
 
+    # parsing 'task' option if present
+    opts.on("-t", "--task STRING", "Run the task with specified index. Overrides any of '-r', '-s', '-u'") do |value|
+        options["skip"] = []
+        options["from"] = value.to_i
+        options["until"] = value.to_i + 1
+    end
+
+    # parsing 'quite' option if present
+    opts.on("-q", "--[no-]quiet", "Quiet mode") do |value|
+        options["quiet"] = value
+    end
+
     # parsing 'help' option if present
     opts.on_tail("-h", "--help", "Show this message") do
         puts opts
@@ -58,21 +90,18 @@ OptionParser.new do |opts|
     end
 end.parse!
 
-options["db_filename"] = ARGV[0] if ARGV.size == 1
-
-scripts_dir = File.join(File.dirname(__FILE__), "db_population")
+scripts_dir = File.join(File.dirname(__FILE__), "../tasks")
 
 Dir.glob(File.join(scripts_dir, "/*.rb")).sort.each do |script|
-    next unless /\d\d_[\w_]+\.rb$/ =~ script
-
-    number = File.basename(script).match(/^\d\d/).to_a[0].to_i
-    next if options["from"] && number < options["from"] 
-    break if options["until"] && number >= options["until"] 
-    next if options['skip'] && options['skip'].include?(number)
-
-    command = "./#{script} -f #{options["db_filename"]}"
-    command << " -m #{options["method"]}" if options["method"]
-    # TODO: output, error_output, exit status, timeouts
-    `#{command}`
+    task_filename = File.basename(script)
+    if /\d{3}_[\w_]+\.rb$/ =~ task_filename
+        options['task_filenames'] << task_filename
+        require_relative script
+    end
 end
+
+filler = Tasks::Scheduler.new(options)
+filler.get_dependencies
+filler.build_dependency_tree
+filler.start_specified_tasks
 
