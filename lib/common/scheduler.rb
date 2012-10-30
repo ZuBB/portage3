041320bf6d2aa86472fce111c13d8e2184905bee
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-10-30 13:30:50 +0200

    Parallel database fill: script class rework
    Changes that were happened with content of the old script class:
     * part that does scripts/tasks management moved into scheduler.rb
     * part that serves task needs moved into runner.rb
     * part that does CLI options querring moved into 03_fill_db.rb
     * part that are common for tasks related stuff moved into tasks.rb

---
+++ lib/common/scheduler.rb
@@ -0,0 +1,190 @@
+#
+# Class for
+#   * parallel processing income data
+#   * passing processed data to the DB queue
+#
+# Filling tables sometimes takes too much time.
+# There is nothing bad in doing this in parallel.
+# For this I use approach quite similar to 'thread pool' pattern
+# http://en.wikipedia.org/wiki/Thread_pool_pattern
+#
+# Initial Author: Vasyl Zuzyak, 04/10/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'thread'
+require 'digest/md5'
+
+require 'runner'
+require 'database'
+require 'plogger'
+require 'utils'
+
+class Tasks::Scheduler
+    SQL = {
+        'check' => 'select count(id) from completed_tasks where name = ?;',
+        'insert' => 'insert or ignore into completed_tasks (name) VALUES (?);'
+    }
+
+    @@shared_data = {}
+    @@semaphore = Mutex.new
+
+    def initialize(params)
+        @id = Digest::MD5.hexdigest(self.class.name + SQL['insert'])
+        @scheduled_tasks = {}
+        @dependency_hash = {}
+        @dependencies = {}
+        @shared_data = {}
+        @options = params
+        @all_tasks = {}
+
+        Database.init(@options['db_filename'])
+        PLogger.init({
+            'path'  => Utils.get_log_home,
+            'dir'   => File.basename(@options['db_filename'], '.sqlite'),
+            'level' => @options["debug"] ? Logger::DEBUG : Logger::INFO
+        })
+
+        PLogger.init_device({'file' => self.class.name, 'id' => @id})
+        Database.validate_query(@id, SQL['insert'])
+
+        Tasks.constants.select { |class_name|
+            class_name.to_s.start_with?(Tasks::TASK_NAME_PREFIX)
+        }.each { |class_name|
+            @all_tasks[class_name.to_s] = Tasks.const_get(class_name)
+        }
+
+        if @options['task_filenames'].size != @all_tasks.size
+            unless @options['quiet']
+                puts "#{diff} task(s) have issue with filename/class name"
+            end
+        end
+    end
+
+    def get_dependencies
+        @all_tasks.each do |name, task|
+            if task.const_defined?('DEPENDS') && !task::DEPENDS.is_a?(String)
+                unless @options['quiet']
+                    puts "Task #{name} has constant 'DEPENDS' of wrong type"
+                end
+                next
+            end
+
+            @dependencies[name] = task.const_defined?('DEPENDS') ?
+                task::DEPENDS.split(/;\ ?/) : []
+
+            @dependencies[name].map! { |dependency|
+                Tasks::TASK_NAME_PREFIX + dependency
+            }
+        end
+    end
+
+    def build_dependency_tree
+        @dependencies.each do |name, deps_array|
+            deps_array.each do |dependency|
+                unless @dependency_hash.has_key?(dependency)
+                    @dependency_hash[dependency] =  []
+                end
+                @dependency_hash[dependency] << name
+            end
+        end
+    end
+
+    def start_specified_tasks
+        Thread.abort_on_exception = true
+
+        get_tasks_by_range.each do |name|
+            thread = Thread.new do
+                PLogger.info(@id, "#{name}: started")
+
+                Thread.current['deps'] = []
+                Thread.current['name'] = name
+                Thread.current['queue'] = Queue.new
+
+                check_task_dependencies
+
+                # TODO other params
+                params = {}
+                params['start'] = Time.now
+                params['name'] = name
+
+                @all_tasks[name].new(params)
+                Database.add_data4insert({'id' => @id, 'data' => name})
+                send_signal2deps(name)
+
+                PLogger.info(@id, "#{name}: I am really done")
+            end
+
+            @scheduled_tasks[name] = thread
+        end
+
+        @scheduled_tasks.values.each { |task| task.join }
+
+        Database.end_of_task(@id)
+        PLogger.end_of_task(@id)
+
+        Database.close
+        PLogger.close
+    end
+
+    private
+    def get_task_by_index
+        @all_tasks.keys.select do |name|
+            name.to_s.match(/^\d{3}/).to_a[0].to_i == options["task"]
+        end
+    end
+
+    def get_tasks_by_range
+        @all_tasks.keys.select do |name|
+            name.to_s.match(/^Task_(\d{3})/).to_a[1].to_i >= @options["from"]
+        end .select do |name|
+            name.to_s.match(/^Task_(\d{3})/).to_a[1].to_i < @options["until"]
+        end .reject do |name|
+            @options['skip'].include?(name.to_s.match(/^Task_(\d{3})/).to_a[1].to_i)
+        end
+    end
+
+    def check_task_dependencies
+        name = Thread.current['name']
+
+        @dependencies[name].each do |dependency|
+            present = Database.get_1value(SQL['check'], dependency) == 1
+            Thread.current['deps'] << dependency if present
+        end
+
+        inaccessible_dependency = @dependencies[name].any? { |dependency|
+            !@scheduled_tasks.has_key?(dependency)
+        }
+
+        if inaccessible_dependency
+            message = "#{name}: unsatisfied dependencies. have to terminate"
+            PLogger.error(@id, message)
+            Thread.exit
+        end
+
+        while Thread.current['deps'].sort != @dependencies[name]
+            Thread.current['deps'] << Thread.current['queue'].pop
+        end
+
+        PLogger.info(@id, "#{name}: passed deps check")
+    end
+
+    def send_signal2deps(name)
+        if @dependency_hash.has_key?(name)
+            @dependency_hash[name].each do |d|
+                if @scheduled_tasks.has_key?(d)
+                    PLogger.info(@id, "#{name} says to #{d}: I am done")
+                    @scheduled_tasks[d]['queue'] << name
+                end
+            end
+        end
+    end
+
+    def self.set_shared_data(key, sql_query)
+        unless @@semaphore.synchronize { @@shared_data.include?(key) }
+            @@semaphore.synchronize {
+                @@shared_data[key] = Hash[Database.select(sql_query)]
+            }
+        end
+    end
+end
+
---
+++ lib/common/tasks.rb
@@ -0,0 +1,9 @@
+module Tasks
+    # should match next regexp ^[A-Z][\w]*
+    TASK_NAME_PREFIX = 'Task_'
+
+    def self.create_task(filename, klass)
+        class_name = "#{TASK_NAME_PREFIX}#{File.basename(filename, '.rb')}"
+        self.const_set(class_name, klass)
+    end
+end
