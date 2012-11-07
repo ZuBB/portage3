88a7ca76fa9a4f6c2d0d87edab46bf08ae604f93
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-07 14:23:03 +0200

    Rework and move of 'content_item_types' script

---
+++ tasks/010_content_item_types.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 01/15/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'installed_package'
+
+klass = Class.new(Tasks::Runner) do
+    self::SQL = {
+        'insert' => 'INSERT INTO content_item_types (type) VALUES (?);'
+    }
+
+    def get_data(params)
+        InstalledPackage::ITEM_TYPES.values
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
+
