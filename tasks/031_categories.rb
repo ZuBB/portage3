86fcd51f9dd7fadf1168db028138ecdab7e55999
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-08 14:00:33 +0200

    Rework and move of 'categories' script

---
+++ tasks/031_categories.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 01/11/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'source'
+require 'category'
+
+klass = Class.new(Tasks::Runner) do
+    self::DEPENDS = '008_sources'
+    self::SOURCE = 'profiles'
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO categories
+            (name, descr, source_id)
+            VALUES (?, ?, ?);
+        SQL
+    }
+
+    def get_data(params)
+        Category.get_categories(params)
+    end
+
+    def get_shared_data
+        Tasks::Scheduler.set_shared_data('source@id', Source::SQL['@'])
+    end
+
+    def process_item(params)
+        PLogger.debug(@id, "Category: #{params}")
+        category = Category.new(params)
+
+        params = [category.category]
+        params << category.category_description
+        params << shared_data('source@id', self.class::SOURCE)
+
+        send_data4insert({'data' => params})
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
+
