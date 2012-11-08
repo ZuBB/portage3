1f5fcd40b05a3f1ef05e4621788e9f687f994bb5
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-08 13:40:23 +0200

    Rework and move of 'repositories' script

---
+++ tasks/021_repositories.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 01/11/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'repository'
+
+klass = Class.new(Tasks::Runner) do
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO repositories
+            (name, parent_folder, repository_folder)
+            VALUES (?, ?, ?);
+        SQL
+    }
+
+    def get_data(params)
+        Repository.get_repositories(params)
+    end
+
+    def process_item(params)
+        PLogger.debug(@id, "Repository: #{params}")
+        repository = Repository.new(params)
+
+        send_data4insert({ 'data' => [
+            repository.repository,
+            repository.repository_pd,
+            repository.repository_fs
+        ]})
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
+
