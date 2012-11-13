2d3fb4b59b42fe279c3a39d2746f26d6b8c7fadc
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-13 12:33:05 +0200

    Rework and move of 'use_flag_types' script

---
+++ tasks/052_use_flag_types.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 04/04/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'useflag'
+
+klass = Class.new(Tasks::Runner) do
+    self::SQL = {
+        'insert' => 'INSERT INTO flag_types (type) VALUES (?);'
+    }
+
+    def get_data(params)
+        UseFlag::TYPES
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
+
