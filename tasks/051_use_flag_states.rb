793fe7f731db2ed55878902b86b10a75a1720805
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-13 12:30:46 +0200

    Rework and move of 'use_flag_states' script

---
+++ tasks/051_use_flag_states.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 01/20/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'useflag'
+
+klass = Class.new(Tasks::Runner) do
+    self::SQL = {
+        'insert' => 'INSERT INTO flag_states (state, status) VALUES (?, ?);'
+    }
+
+    def get_data(params)
+        UseFlag::STATES
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
+
