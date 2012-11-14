820f2a0389e72ef1b483a7523e7d6c5dcd27c926
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-14 12:53:30 +0200

    Rework and move of 'ebuild_homepages_p2' script

---
+++ tasks/100_ebuild_homepages.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 07/25/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+
+klass = Class.new(Tasks::Runner) do
+    self::DEPENDS = '099_ebuild_homepages'
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO ebuilds_homepages
+            (homepage_id, ebuild_id)
+            VALUES (?, ?);
+        SQL
+    }
+
+    def get_data(params)
+        sql_query = <<-SQL
+            SELECT eh.id, teh.ebuild_id
+            FROM ebuild_homepages eh
+            JOIN tmp_ebuild_homepages teh ON eh.homepage = teh.homepage;
+        SQL
+        Database.select(sql_query)
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
+
