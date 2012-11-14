b096209e7e4a487716c8b4066e3c5944fe282788
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-14 13:25:45 +0200

    Added stub code for 'eapi' module

---
+++ lib/portage/eapi.rb
@@ -0,0 +1,12 @@
+#
+# Library supporting analytics scripts running on CloudDB
+#
+# Initial Author: Vasyl Zuzyak, 04/02/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+class Eapi
+    SQL = {
+        '@' => 'select version, id from eapis;'
+    }
+end
+
