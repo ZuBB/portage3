f2eda41c132ef7773f4f5112baff18fd68482ce9
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-14 12:46:49 +0200

    Rework, split and move of 'ebuild_homepages_p1' script

---
+++ tasks/098_ebuild_homepages.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 04/20/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'ebuild'
+
+klass = Class.new(Tasks::Runner) do
+    self::DEPENDS = '091_ebuilds'
+    self::THREADS = 4
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO tmp_ebuild_homepages
+            (homepage, ebuild_id)
+            VALUES (?, ?);
+        SQL
+    }
+
+    def get_data(params)
+        Ebuild.get_ebuilds(params)
+    end
+
+    def process_item(params)
+        PLogger.debug(@id, "Ebuild: #{params[3, 3].join('-')}")
+        ebuild = Ebuild.new(Ebuild.generate_ebuild_params(params))
+
+        ebuild.ebuild_homepage.split.each { |homepage|
+            send_data4insert({'data' => [homepage, ebuild.ebuild_id]})
+        }
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
+
