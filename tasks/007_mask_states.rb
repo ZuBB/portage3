2b96f681e411519be0a8b91cf76385788a647734
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-15 22:54:15 +0200

    Use stuff from Mask module

--- tasks/007_mask_states.rb
+++ tasks/007_mask_states.rb
@@ -6,14 +6,15 @@
 # Initial Author: Vasyl Zuzyak, 01/31/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
+require 'mask'
+
 klass = Class.new(Tasks::Runner) do
-    self::MASK_STATES = ['masked', 'unmasked']
     self::SQL = {
         'insert' => 'INSERT INTO mask_states (state) VALUES (?);'
     }
 
     def get_data(params)
-        self.class::MASK_STATES
+        Mask::STATES
     end
 end
 
