ee4a4ff710c6d96fd395f8f17f712de570687ce5
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-22 21:57:30 +0200

    Few small improvements into useflag lib

--- lib/portage/useflag.rb
+++ lib/portage/useflag.rb
@@ -82,28 +82,26 @@ class UseFlag
         end
     end
 
-    def self.expand_asterix_flag(line, package_id)
+    def self.expand_asterix_flag(flags, package_id)
         sql_query = <<-SQL
             select distinct f.name
             from flags f
             join flags_states fs on fs.flag_id = f.id
             join ebuilds e on e.id = fs.ebuild_id
-            where e.package_id = ?;
+            where e.package_id = ? and (f.type_id BETWEEN 4 and 6);
         SQL
         all_flags = Database.select(sql_query, package_id).flatten
-
-        flags = line.split.drop(1)
 
         asterix_index = flags.index { |i| i.include?('*') }
         asterix_flag = flags.delete_at(asterix_index)
         asterix_state = self.get_flag_state(asterix_flag)
         asterix_state_sign = asterix_state == 'disabled' ? '-' : ''
 
-        flag_names = flags.map { |f| self.get_flag(f) }
-        new_flags = all_flags - flag_names
+        old_flags = flags.map { |f| self.get_flag(f) }
+        new_flags = all_flags - old_flags
         new_flags.map! { |f| f.insert(0, asterix_state_sign) }
 
-        flags.insert(asterix_index, new_flags.join(' ')).join(' ')
+        flags.insert(asterix_index, *new_flags)
     end
 end
 
