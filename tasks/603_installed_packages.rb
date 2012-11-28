bce321b22f19b1ac92e20a7cf2c36adaa4b83292
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-28 17:28:31 +0200

    Move and rework of '81_installed_packages_p2' script

---
+++ tasks/603_installed_packages.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 03/23/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'source'
+require 'installed_package'
+
+klass = Class.new(Tasks::Runner) do
+    self::DEPENDS = '008_sources'
+    self::SOURCE = '/var/db/pkg'
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO tmp_installed_packages_categories
+            (name, source_id)
+            VALUES (?, ?);
+        SQL
+    }
+
+    def get_data(params)
+        Dir[File.join(InstalledPackage::DB_PATH, '*/*/')]
+    end
+
+    def set_shared_data
+        request_data('source@id', Source::SQL['@'])
+    end
+
+    def process_item(item)
+        if (category = InstalledPackage.get_file_content(item, 'CATEGORY'))
+            send_data4insert({'data' => [
+                category,
+                shared_data('source@id', self.class::SOURCE),
+            ]})
+        else
+            @logger.error("File `#{item}CATEGORY` is missed")
+        end
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
