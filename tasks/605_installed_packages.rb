8846aa9965e04d21efe2c1fb63211d4474206a46
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-29 13:03:32 +0200

    Move and rework 81_installed_packages_p3 script

---
+++ tasks/605_installed_packages.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 03/23/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'source'
+require 'category'
+require 'installed_package'
+
+klass = Class.new(Tasks::Runner) do
+    self::DEPENDS = '604_installed_packages'
+    self::SOURCE = '/var/db/pkg'
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO tmp_installed_packages_packages
+            (name, category_id, source_id)
+            VALUES (?, ?, ?);
+        SQL
+    }
+
+    def get_data(params)
+        Dir[File.join(InstalledPackage::DB_PATH, '*/*/')]
+    end
+
+    def set_shared_data
+        request_data('category@id', Category::SQL['@'])
+        request_data('source@id', Source::SQL['@'])
+    end
+
+    def process_item(item)
+        category = InstalledPackage.get_file_content(item, 'CATEGORY')
+        # NOTE for some reason Portage does not create 'PACKAGE' files...
+        pf = InstalledPackage.get_file_content(item, 'PF')
+
+        if pf && category
+            send_data4insert({'data' => [
+                Atom.get_package(pf),
+                shared_data('category@id', category),
+                shared_data('source@id', self.class::SOURCE)
+            ]})
+        else
+            @logger.error("File `#{item}PACKAGE` is missed")
+        end
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
---
+++ tasks/606_installed_packages.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 03/23/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'package'
+
+klass = Class.new(Tasks::Runner) do
+    self::DEPENDS = '154_profile_masks;605_installed_packages'
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO packages
+            (name, category_id, source_id)
+            VALUES (?, ?, ?);
+        SQL
+    }
+
+    def get_data(params)
+        sql_query = Package::SQL['ghost'].dup
+        sql_query.sub!('TMP_TABLE', 'tmp_installed_packages_packages')
+        Portage3::Database.get_client.select(sql_query)
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
