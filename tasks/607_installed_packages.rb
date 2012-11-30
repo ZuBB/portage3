a2cff5477f23d7266305fd3f59d4f5f090dff876
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-11-30 10:29:58 +0200

    Move and rework of 81_installed_packages_p4 script

---
+++ tasks/607_installed_packages.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 03/23/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'atom'
+require 'source'
+require 'repository'
+require 'installed_package'
+
+klass = Class.new(Tasks::Runner) do
+    self::DEPENDS = '606_installed_packages'
+    self::SOURCE = '/var/db/pkg'
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO tmp_installed_packages_ebuilds
+            (version, package_id, repository_id, /*slot,*/ source_id)
+            VALUES (?, ?, ?, /*?,*/ ?);
+        SQL
+    }
+
+    def get_data(params)
+        Dir[File.join(InstalledPackage::DB_PATH, '*/*/')]
+    end
+
+    def set_shared_data
+        request_data('repository@id', Repository::SQL['@'])
+        request_data('source@id', Source::SQL['@'])
+        request_data('CPN@id', Atom::SQL['@1'])
+    end
+
+    def process_item(item)
+        category = InstalledPackage.get_file_content(item, 'CATEGORY')
+        repo = InstalledPackage.get_file_content(item, 'repository')
+       #slot = InstalledPackage.get_file_content(item, 'SLOT')
+        pf = InstalledPackage.get_file_content(item, 'PF')
+        package = Atom.get_package(pf)
+        version = Atom.get_version(pf)
+
+        send_data4insert({'data' => [
+            version,
+            shared_data('CPN@id', category + '/' + package),
+            shared_data('repository@id', repo),
+           #slot,
+            shared_data('source@id', self.class::SOURCE),
+        ]})
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
---
+++ tasks/608_installed_packages.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 03/23/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+require 'ebuild'
+
+klass = Class.new(Tasks::Runner) do
+    self::DEPENDS = '091_ebuilds;412_users_mask;607_installed_packages'
+    self::SQL = {
+        'insert' => <<-SQL
+            INSERT INTO ebuilds
+            (version, package_id, repository_id, /*slot,*/ source_id)
+            VALUES (?, ?, ?, /*?,*/ ?);
+        SQL
+    }
+
+    def get_data(params)
+        sql_query = Ebuild::SQL['ghost'].dup
+        sql_query.sub!('TMP_TABLE', 'tmp_installed_packages_ebuilds')
+        Portage3::Database.get_client.select(sql_query)
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
