42feba0e675222d5b244a7a6f76a3cad513e68f4
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-12-23 22:28:46 +0200

    Added new namespace: Portage3
    This namespace should hold all modules/classes that were/are/will be created

---
+++ lib/portage3.rb
@@ -0,0 +1,22 @@
+#
+# Library supporting analytics scripts running on CloudDB
+#
+# Initial Author: Vasyl Zuzyak, 01/05/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+
+require 'thread'
+require 'digest/md5'
+
+module Portage3
+    # TODO fill this module
+end
+
+Thread.abort_on_exception = true
+
+require 'common/database'
+require 'common/logger'
+require 'common/utils'
+
+# TODO this is a temporary solution
+require 'common/parser'
