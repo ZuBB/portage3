7ee4c9bafb9aed02771fc99a766a45cea11e10b8
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-12-25 02:22:28 +0200

    ebuild_homepages: reindex + ebuild one read feature

--- tasks/107_ebuild_homepages.rb
+++ tasks/107_ebuild_homepages.rb
@@ -8,16 +8,15 @@
 #
 
 klass = Class.new(Tasks::Runner) do
-    self::DEPENDS = '098_ebuild_homepages'
+    self::DEPENDS = '106_ebuild_homepages'
     self::SQL = {
         'insert' => 'INSERT INTO ebuild_homepages (homepage) VALUES (?);'
     }
 
     def get_data(params)
         sql_query = 'SELECT distinct homepage FROM tmp_ebuild_homepages;'
-        Database.select(sql_query).flatten
+        Portage3::Database.get_client.select(sql_query).flatten
     end
 end
 
 Tasks.create_task(__FILE__, klass)
-
