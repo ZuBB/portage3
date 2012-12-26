5913e3bba46f664161f1703e989bb43d6f335c10
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-12-26 10:10:55 +0200

    059_use_flag_stuff: fixed typo in dependencies
    // хто спішить той людей смішить!

--- tasks/059_use_flag_stuff.rb
+++ tasks/059_use_flag_stuff.rb
@@ -9,12 +9,12 @@
 
 klass = Class.new(Tasks::Runner) do
     # metatask that depends on all flags stuff
-    self::DEPENDS = '053_use_flag_basic_stuff.rb;'\
-                    '054_global_use_flags.rb;'\
-                    '055_local_use_flags.rb;'\
-                    '056_expand_use_flags.rb;'\
-                    '057_hidden_use_flags.rb;'\
-                    '058_arch_use_flags.rb;'
+    self::DEPENDS = '053_use_flag_basic_stuff;'\
+                    '054_global_use_flags;'\
+                    '055_local_use_flags;'\
+                    '056_expand_use_flags;'\
+                    '057_hidden_use_flags;'\
+                    '058_arch_use_flags;'
 
     self::SQL = {
         'insert' => 'INSERT INTO flag_states (state, status) VALUES (?, ?);'
