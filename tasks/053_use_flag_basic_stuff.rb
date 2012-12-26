74586a993eeedc2ea55e7ce2b3a2e2be84f33c3f
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-12-26 08:47:43 +0200

    053_use_flag_basic_stuff: new task
    Metatask. depends on basic params for flags: state and type
    (051_use_flag_states and 052_use_flag_types respectively)

---
+++ tasks/053_use_flag_basic_stuff.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Here should go some comment
+#
+# Initial Author: Vasyl Zuzyak, 01/20/12
+# Latest Modification: Vasyl Zuzyak, ...
+#
+
+klass = Class.new(Tasks::Runner) do
+    # metatask that depends on basic flags stuff
+    self::DEPENDS = '051_use_flag_states;052_use_flag_types'
+    self::SQL = {
+        'insert' => 'INSERT INTO flag_states (state, status) VALUES (?, ?);'
+    }
+
+    def get_data(params)
+        []
+    end
+end
+
+Tasks.create_task(__FILE__, klass)
+
