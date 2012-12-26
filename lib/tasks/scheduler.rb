416b32c0b2ddd8c8760f1374dd57f2cd16e4ca71
Vasyl Zuzyak <zvasylvv@gmail.com>
2012-12-26 15:11:09 +0200

    scheduler

--- lib/tasks/scheduler.rb
+++ lib/tasks/scheduler.rb
@@ -17,6 +17,7 @@ Encoding.default_internal = 'UTF-8'
 
 class Tasks::Scheduler
     TAKS_INDEX = /^Task_(\d{3})/
+    PORT       = 8134
     SQL = {
         'check' => 'select count(id) from completed_tasks where name = ?;',
     }
@@ -28,12 +29,13 @@ class Tasks::Scheduler
     def initialize(params)
         @db_client   = nil
         @options    = params
+       #@shared_data = {}
         @start_time = Time.now
         @task_specs = {
             'all'   => {},
             'deps'  => {},
             'rdeps' => {},
-            'trds'  => {},
+            'pids'  => {},
             '2run'  => [],
             '4run'  => []
         }
@@ -56,35 +58,35 @@ class Tasks::Scheduler
             return false
         end
 
-        @task_specs['2run'].each do |name|
-            @task_specs['trds'][name] = Thread.new do
-                @logger.info("#{name}: started")
+        while @task_specs['2run'].size > 0
+            @task_specs['2run'].map! do |name|
+                # if task does not have deps - run it
+                if @task_specs['deps'][name].nil?
+                    name = run_task(name)
+                # if task has deps and they were satisfied on prev session
+                elsif deps_satisfied_by_prev_session?(name)
+                    name = run_task(name)
+                # if task's deps already not running
+                elsif !deps_already_finished?(name)
+                    # and all deps satisfied - lets run it
+                    if task_dependencies_satisfied?(name)
+                        name = run_task(name)
+                    end
+                end
+                # TODO runtime dead deps
 
-                Thread.current['deps'] = []
-                Thread.current['name'] = name
-                Thread.current['queue'] = Queue.new
-                Thread.current['conn'] = Portage3::Database.get_client
-
-                check_task_dependencies(name)
-
-                # TODO other params
-                params = {}
-                params['name'] = name
-                params['connection'] = Thread.current['conn']
-
-                @task_specs['all'][name].new(params)
-                send_signal2deps(name)
-
-                @logger.info("#{name}: I am really done")
+                name
             end
+            @task_specs['2run'].compact!
+            sleep(0.5) unless @task_specs['2run'].empty?
         end
     end
 
     def finalize
-        @task_specs['trds'].values.each { |task|
-            @logger.info("#{task['name']}: attempt to do a 'join'")
-            task.join
-            @logger.info("#{task['name']}: attempt successful")
+        @task_specs['pids'].values.each { |pid|
+            if self.class.process_running?(pid)
+                Process.waitpid(pid, 0)
+            end
         }
 
         @db_client.shutdown_server
@@ -92,6 +94,8 @@ class Tasks::Scheduler
         @logger.info("Total passed: #{(Time.now - @start_time)} seconds")
         @logger.finish_logging
         Portage3::Logger.shutdown_server
+
+       #@server_thread.kill
     end
 
     private
@@ -111,6 +115,19 @@ class Tasks::Scheduler
             'file' => self.class.name,
             'id'   => @id
         })
+
+if false
+        # shared_data server
+        server = TCPServer.new('localhost', PORT)
+        Thread::abort_on_exception = true
+        @server_thread = Thread.new do
+            loop do
+                Thread.start(server.accept) do |connection|
+                    process_connection(connection)
+                end
+            end
+end
+        end
     end
 
     def account_all_tasks
@@ -151,47 +168,6 @@ class Tasks::Scheduler
         end
     end
 
-    def get_task_by_index
-        @all_tasks.keys.select do |name|
-            name.to_s.match(/^\d{3}/).to_a[0].to_i == options["task"]
-        end
-    end
-
-    def self.get_task_index(symbol)
-        symbol.to_s.match(TAKS_INDEX).to_a[1].to_i
-    end
-
-    def get_tasks_by_range
-        # TODO fix bug with 0 tasks to run
-        _from  = @options['from']
-        _skip  = @options['skip']
-        _until = @options['until']
-
-        @task_specs['4run'] = @task_specs['all']
-        .keys
-        .select { |name| self.class.get_task_index(name) >= _from }
-        .select { |name| self.class.get_task_index(name) < _until }
-        .reject { |name| _skip.include?(self.class.get_task_index(name)) }
-    end
-
-    def check_task_dependencies(name)
-        @logger.info("#{name}: checking dependencies")
-
-        if @task_specs['deps'].has_key?(name)
-            @task_specs['deps'][name].each do |dependency|
-                db_client = Thread.current['conn']
-                count = db_client.get_1value(SQL['check'], dependency)
-                Thread.current['deps'] << dependency if count == 1
-            end
-
-            while Thread.current['deps'].sort != @task_specs['deps'][name]
-                Thread.current['deps'] << Thread.current['queue'].pop
-            end
-        end
-
-        @logger.info("#{name}: passed dependency check")
-    end
-
     def dead_dependencies?
         all_required_dependencies = []
 
@@ -211,6 +187,30 @@ class Tasks::Scheduler
         }
     end
 
+    def deps_satisfied_by_prev_session?(name)
+        scheduled = @task_specs['deps'][name].any? { |dependency|
+            @task_specs['4run'].include?(dependency)
+        }
+
+        return false if scheduled
+        task_dependencies_satisfied?(name)
+    end
+
+    def deps_already_finished?(name)
+        @task_specs['deps'][name].all? { |task|
+            Tasks::Scheduler.process_finished?(@task_specs['pids'][task])
+        }
+    end
+
+    def task_dependencies_satisfied?(name)
+        satisfied_dependencies = get_tasks_satisfied_dependencies([name])
+
+        result = satisfied_dependencies.sort == @task_specs['deps'][name]
+        @logger.info("#{name}: passed deps check") if result
+
+        result
+    end
+
     def get_tasks_satisfied_dependencies(task_names = @task_specs['2run'])
         satisfied_dependencies = []
 
@@ -226,15 +226,16 @@ class Tasks::Scheduler
         satisfied_dependencies.uniq
     end
 
-    def send_signal2deps(name)
-        return unless @task_specs['rdeps'].has_key?(name)
-
-        @task_specs['rdeps'][name].each do |d|
-            if @task_specs['trds'].has_key?(d)
-                @logger.info("#{name} says to #{d}: I am done")
-                @task_specs['trds'][d]['queue'] << name
-            end
+    def run_task(name)
+        @logger.info("#{name}: will start in a moment")
+        @task_specs['pids'][name] = fork do
+            @task_specs['all'][name].new({
+                'name'        => name,
+                'debug'       => @options['debug'],
+                'db_filename' => @options['db_filename']
+            })
         end
+        nil
     end
 
     def expand_skip_param
@@ -251,6 +252,78 @@ class Tasks::Scheduler
         end
         .flatten
         .map { |i| i.to_i }
+    end
+
+    def expand_task_by_index
+        #@all_tasks.keys.select do |name|
+            #name.to_s.match(/^\d{3}/).to_a[0].to_i == options["task"]
+        #end
+    end
+
+    def get_tasks_by_range
+        # TODO fix bug with 0 tasks to run
+        _from  = @options['from']
+        _skip  = @options['skip']
+        _until = @options['until']
+
+        @task_specs['4run'] = @task_specs['all']
+        .keys
+        .select { |name| self.class.get_task_index(name) >= _from }
+        .select { |name| self.class.get_task_index(name) < _until }
+        .reject { |name| _skip.include?(self.class.get_task_index(name)) }
+    end
+
+    def process_connection(connection)
+        required_keys = ['key', 'query']
+
+        while accepted_string = connection.gets
+            begin
+                json_object = JSON.parse(accepted_string)
+            rescue
+                @logger.error('Failed to parse next incoming string')
+                @logger.info(accepted_string)
+                next
+            end
+
+            unless required_keys.all? { |key| json_object.has_key?(key) }
+                @logger.warn('incoming object does not have all params')
+                next
+            end
+
+            key   = json_object['key']
+            query = json_object['query']
+
+            have_data = @mutex.synchronize {
+                @shared_data.include?(key)
+            }
+
+            unless have_data
+                @mutex.synchronize {
+                    @shared_data[key] = Hash[@database.get({
+                        'action' => 'get_select',
+                        'params' => [query]
+                    })]
+                }
+            end
+
+            connection.puts(JSON.generate({'result' => @shared_data[key]}))
+        end
+    end
+
+    def self.get_task_index(symbol)
+        symbol.to_s.match(TAKS_INDEX).to_a[1].to_i
+    end
+
+    def self.process_running?(pid)
+        (Process.waitpid(pid, Process::WNOHANG) rescue false).nil?
+    end
+
+    def self.process_finished?(pid)
+        (Process.kill(0, pid) rescue 0).zero?
+    end
+
+    def self.create_client_socket
+        TCPSocket.open("localhost", PORT)
     end
 
     def self.set_shared_data(key, sql_query)
