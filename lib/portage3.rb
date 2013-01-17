29493d9320e1a30a5c058a72b0e970bbe56a351e
Vasyl Zuzyak <zvasylvv@gmail.com>
2013-01-17 10:28:55 +0200

    portage3: add 'settings_home' method
    'settings_home' returns path to the main 'make.conf' file

--- lib/portage3.rb
+++ lib/portage3.rb
@@ -9,7 +9,13 @@ require 'thread'
 require 'digest/md5'
 
 module Portage3
-    # TODO fill this module
+    def self.settings_home
+        old_path = '/etc/make.conf'
+        new_path = '/etc/portage/make.conf'
+
+        return old_path if File.exist?(old_path)
+        return new_path if File.exist?(new_path)
+    end
 end
 
 Thread.abort_on_exception = true
