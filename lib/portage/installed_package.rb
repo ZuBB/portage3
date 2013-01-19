0e386e5b86583da519a3a8958b1b329e2544e2ec
Vasyl Zuzyak <zvasylvv@gmail.com>
2013-01-19 12:53:41 +0200

    installed_package lib: improvements for package content stuff

--- lib/portage/installed_package.rb
+++ lib/portage/installed_package.rb
@@ -4,6 +4,8 @@
 # Initial Author: Vasyl Zuzyak, 01/05/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
+require 'pathname'
+
 module InstalledPackage
     DB_PATH = '/var/db/pkg'
     ITEM_TYPES = {
@@ -12,9 +14,26 @@ module InstalledPackage
         'symlink'   => 'sym'
     }
     SQL = {
-        'content_type_id' => 'SELECT id FROM content_item_types where type=?;',
-        'item_id' => 'select id from ipackage_content where item=?'
+        '@1' => 'SELECT type, id FROM content_item_types;',
+        '@2' => <<-SQL
+            SELECT item, id
+            FROM ipackage_content
+            WHERE type_id = (
+                SELECT id
+                FROM content_item_types
+                WHERE type = '#{ITEM_TYPES['file']}'
+            );
+        SQL
     }
+    SQL['@3'] = <<-SQL
+        SELECT item, id
+        FROM ipackage_content
+        WHERE type_id = (
+            SELECT id
+            FROM content_item_types
+            WHERE type = '#{ITEM_TYPES['symlink']}'
+        );
+    SQL
 
     def self.get_data(params)
         sql_query = <<-SQL
@@ -25,22 +44,22 @@ module InstalledPackage
             JOIN categories c ON p.category_id = c.id;
         SQL
 
-        Database.select(sql_query)
+        Portage3::Database.get_client.select(sql_query)
     end
 
-    def self.get_file(param, file)
+    def self.get_file(param, file, silent = false)
         _PF = param[2] + '-' + param[3]
         dir = File.join(InstalledPackage::DB_PATH, param[1], _PF)
 
         if !File.exist?(dir) || !File.directory?(dir)
-            PLogger.error("Dir '#{dir}' is missed in '#{DB_PATH}'")
+            PLogger.error("Dir '#{dir}' is missed in '#{DB_PATH}'") unless silent
             return false
         end
 
         filepath = File.join(dir, file)
 
         if !File.exist?(filepath) || !File.file?(filepath)
-            PLogger.info("File '#{file}' is missed in '#{filepath}'")
+            PLogger.info("File '#{file}' is missed in '#{filepath}'") unless silent
             return false
         end
 
@@ -59,42 +78,35 @@ module InstalledPackage
     end
 
     def self.get_file_content(path_part, filename)
+        method_name = path_part.is_a?(Array) ? 'get_file' : 'get_filepath'
+        filepath = self.send(method_name, path_part, filename, true)
         content = nil
-        filepath = self.get_filepath(path_part, filename, true)
 
-		if filepath.is_a?(String) && File.readable?(filepath)
-			content = IO.read(filepath).strip
+        if filepath.is_a?(String) && File.readable?(filepath)
+            content = IO.read(filepath).strip
 
-			if !content.is_a?(String) || content.empty?
-				PLogger.warn("File '#{filepath}' has invalid content")
-				return content
-			end
-		end
+            if !content.is_a?(String) || content.empty?
+                # TODO PLogger.warn("File '#{filepath}' has invalid content")
+                return content
+            end
+        end
 
         content
     end
 
-    def self.get_symlink_id(line, cline)
-        parts = line.split('->').map { |i| i.strip }
-        if parts.size != 2
-            PLogger.group_log([
-                [3, 'Its something wrong with next item'],
-                [1, cline],
-            ])
-            return false
-        end
-
-        item_dir = File.dirname(parts[0])
-        symlink_target = File.expand_path(File.join(item_dir, parts[1]))
-        symlink_id = Database.get_1value(SQL['item_id'], symlink_target)
-        [parts[0], symlink_id]
+    def self.get_file_lines(path_part, filename)
+        content = self.get_file_content(path_part, filename)
+        content.is_a?(String) ? content.lines : []
     end
 
-    def self.content_post_insert_check(item_type)
-        {
-            item_type =>
-                Database.get_1value(SQL['content_type_id'], item_type)
-        }
+    def self.symlink_target(parts)
+        if Pathname.new(parts[1]).absolute?
+            parts[1]
+        else
+            path = File.join(File.dirname(parts[0]), parts[1])
+            path = File.expand_path(path) if parts[1].start_with?('.')
+            path
+        end
     end
 end
 
