8f6cc37dbd90c739613724c77d7775941c986bb8
Vasyl Zuzyak <zvasylvv@gmail.com>
2013-01-28 17:10:54 +0200

    profiles lib: couple of minor changes
    They are:
     * now profiles lib is a module (used to be a class)
     * added new constant (those const its an array of filenames from
        profiles dir that contain atoms and package categories
        correspondingly)
     * added new method walks through all files from 'profiles' dir that
        may contain package atoms

--- lib/portage/profiles.rb
+++ lib/portage/profiles.rb
@@ -4,10 +4,34 @@
 # Initial Author: Vasyl Zuzyak, 04/02/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
-class PProfile
+module PProfile
     SQL = {
         '@' => 'SELECT name, id FROM profiles;',
         'names' => 'SELECT name from profiles;'
     }
+    # find $(ls -d */ | grep -vE 'updates|desc') -type f -printf "%f\n" | sort -u
+    FILES_WITH_ATOMS = [
+        "package.keywords",
+        "package.mask",
+        "package.unmask",
+        "package.provided",
+        "package.use",
+        "package.use.force",
+        "package.use.mask",
+        "package.use.stable.force",
+        "package.use.stable.mask",
+        "packages",
+        "packages.build",
+    ]
+
+    def self.files_with_atoms(params)
+        FILES_WITH_ATOMS.map { |filename|
+            Dir[File.join(params['profiles_home'], "**/#{filename}")]
+        }
+        .flatten
+        .reject { |item|
+            File.exist?(File.join(File.dirname(item), 'deprecated'))
+        }
+    end
 end
 
