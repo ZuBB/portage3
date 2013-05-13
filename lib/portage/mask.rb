9b0c583a1b15f2003f1d5efd19420690f9e24546
Vasyl Zuzyak <zvasylvv@gmail.com>
2013-05-13 16:35:05 +0300

    lib/portage/mask.rb

--- lib/portage/mask.rb
+++ lib/portage/mask.rb
@@ -4,7 +4,6 @@
 # Initial Author: Vasyl Zuzyak, 04/02/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
-require 'atom'
 
 class Mask
     STATES = ['masked', 'unmasked']
@@ -12,41 +11,10 @@ class Mask
         '@' => 'SELECT state, id FROM mask_states;'
     }
 
-    def self.parse_line(line)
-        result = {}
-
+    def self.get_mast_state(str)
         # take care about leading '-'
         # it means this atom/package should treated as unmasked
-        result["state"] = (line.slice!(/^-/) == '-') ? STATES[1] : STATES[0]
-
-        # take care about trailing ':'
-        # it means slot for this atom/package
-        result["slot"] = line.slice!(/(?=:).+$/)[1..-1] if line.include?(':')
-
-        # take care about leading ~
-        # it means match any subrevision of the specified base version.
-        if line.start_with?('~')
-            line.sub!(/^~/, '')
-            line << '*' unless line.end_with?('*')
-        end
-
-        # get version restrictions
-        result["vrestr"] = line.slice!(Atom::VERSION_RESTRICTION)
-
-        # get versions
-        unless (version = Atom.get_version(line)).nil?
-            result["version"] = version
-            line.sub!('-' + version, '')
-        end
-
-        if result["vrestr"].nil? && /\*$/ =~ result["version"]
-            result["vrestr"]  = '='
-        end
-
-        result['atom'] = line
-        result["category"], result["package"] = *line.split('/')
-
-        result
+        str == '-' ? STATES[1] : STATES[0]
     end
 end
 
