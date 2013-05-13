8e77f1ec058307f855a1c8197713fceeb03f62c8
Vasyl Zuzyak <zvasylvv@gmail.com>
2013-05-13 16:36:05 +0300

    161_profile_masks

--- tasks/161_profile_masks.rb
+++ tasks/161_profile_masks.rb
@@ -6,98 +6,67 @@
 # Initial Author: Vasyl Zuzyak, 01/26/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
-require 'atom'
 require 'mask'
-require 'source'
-require 'profiles'
 
 klass = Class.new(Tasks::Runner) do
     self::DEPENDS = '006_profiles;007_mask_states;157_profile_ebuild_versions'
+    self::TARGETS = ['package.mask']
+    self::SOURCE  = 'profiles'
     self::THREADS = 4
-    self::SOURCE = 'profiles'
     self::SQL = {
         'insert' => <<-SQL
             INSERT INTO ebuilds_masks
-            (ebuild_id, state_id, profile_id, source_id)
-            VALUES (?, ?, ?, ?);
+            (ebuild_id, state_id, arch_id, profile_id, source_id)
+            VALUES (?, ?, ?, ?, ?);
         SQL
     }
 
     def get_data(params)
-        Portage3::Database.get_client.select(PProfile::SQL['names']).flatten
+        Portage3::Database.get_client.select(
+            Portage3::Profile::SQL['names']).flatten
     end
 
     def set_shared_data
-        request_data('profile@id', PProfile::SQL['@'])
-        request_data('mask_state@id', Mask::SQL['@'])
+        request_data('profile@id', Portage3::Profile::SQL['@'])
         request_data('source@id', Source::SQL['@'])
+        request_data('profile@arch_id', Portage3::Profile::SQL['@2'])
+        request_data('mask_state@id', Mask::SQL['@'])
         request_data('CPN@id', Atom::SQL['@1'])
     end
 
-    def process_file(filename, profile)
-        @logger.debug("File: #{filename}")
-
-        IO.foreach(filename) do |line|
-            next if /^\s*#/ =~ line
-            next if /^\s*$/ =~ line
-
-            result = Mask.parse_line(line.strip)
-
-            if (result['package_id'] = shared_data('CPN@id', result['atom'])).nil?
-                @logger.warn("File `#{filename}` has dead package: #{line.strip}")
-                next
-            end
-
-            if (result_set = Atom.get_ebuilds(result)).size == 0
-                @logger.warn("File `#{filename}` has dead PV: #{line.strip}")
-                next
-            end
-
-            result_set.each { |ebuild_id|
-                send_data4insert({'data' => [
-                    ebuild_id,
-                    shared_data('mask_state@id', result["state"]),
-                    shared_data('profile@id', profile),
-                    shared_data('source@id', self.class::SOURCE)
-                ]})
-            }
-        end
-    end
-
-    def process_dir(path, profile)
-        return if File.exist?(File.join(path, 'deprecated'))
-
-        # we need to add base dir of profile also as source of data
-        if File.size?(new_parent = File.join(path, 'parent'))
-            IO.read(new_parent).lines.each do |relative_path|
-                next if /^\s*#/ =~ relative_path
-                next if /^\s*$/ =~ relative_path
-                relative_path.strip!
-
-                new_path = File.join(path, relative_path)
-                process_dir(File.realpath(new_path), profile)
-            end
-        end
-
-        if File.exist?(package_mask_file = File.join(path, 'package.mask'))
-            process_file(package_mask_file, profile)
-        end
-    end
-
     def process_item(profile)
-        profile_path = File.join(Utils::get_profiles_home, profile)
+        Portage3::Profile.process_profile_dirs(profile, self.class::TARGETS)
+        .uniq
+        .each { |filename|
+            IO.readlines(filename)
+            .reject { |line| /^\s*#/ =~ line }
+            .reject { |line| /^\s*$/ =~ line }
+            .map    { |line| line.strip }
+            .each   { |line|
+                result = Atom.parse_atom_string(line)
+                result['package_id'] = shared_data('CPN@id', result['atom'])
 
-        unless File.exist?(profile_path)
-            @logger.error("Path #{profile_path} does not exist")
-            return
-        end
+                if result['package_id'].nil?
+                    @logger.warn("File `#{filename}` has dead package: #{line}")
+                    next
+                end
 
-        if File.file?(profile_path)
-            @logger.error("Path #{profile_path} is a file")
-            return
-        end
+                if (result_set = Atom.get_ebuilds(result)).empty?
+                    @logger.warn("File `#{filename}` has dead PV: #{line}")
+                    next
+                end
 
-        process_dir(profile_path, profile)
+                result_set.each { |ebuild_id|
+                    send_data4insert({'data' => [
+                         ebuild_id,
+                         shared_data('mask_state@id', Mask.get_mast_state(result['prefix'])),
+                         shared_data('profile@arch_id', profile),
+                         shared_data('profile@id', profile),
+                         shared_data('source@id', self.class::SOURCE)
+                    ]})
+                }
+            }
+        }
     end
 end
 
