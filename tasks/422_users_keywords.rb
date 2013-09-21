5413a863a7c2387d3c8caf333f77835fcc1ee88c
Vasyl Zuzyak <zvasylvv@gmail.com>
2013-09-21 23:50:21 +0300

    42{1,2}_users_keywords: rename & improve

--- tasks/422_users_keywords.rb
+++ tasks/422_users_keywords.rb
@@ -6,10 +6,6 @@
 # Initial Author: Vasyl Zuzyak, 02/02/12
 # Latest Modification: Vasyl Zuzyak, ...
 #
-require 'atom'
-require 'source'
-require 'keyword'
-require 'setting'
 
 klass = Class.new(Tasks::Runner) do
     self::DEPENDS = '091_ebuilds'
@@ -28,8 +24,10 @@ klass = Class.new(Tasks::Runner) do
     end
 
     def set_shared_data
+        request_data('keyword@id', Portage3::Keyword::SQL['@2'])
         request_data('setting@id', Portage3::Setting::SQL['@'])
         request_data('source@id', Source::SQL['@'])
+        request_data('arch@id', Portage3::Keyword::SQL['@1'])
         request_data('CPN@id', Atom::SQL['@1'])
     end
 
@@ -38,16 +36,16 @@ klass = Class.new(Tasks::Runner) do
         return if /^\s*$/ =~ line
 
         arch = shared_data('setting@id', 'arch')
-        keyword = shared_data('setting@id', 'keyword')
-        result = Keyword.parse_line(line.strip, arch, keyword)
 
-        
+        result = Atom.parse_atom_string(line.strip)
+        result2 = Portage3::Keyword.parse_line(result['arch'], arch)
+
         if (result['package_id'] = shared_data('CPN@id', result['atom'])).nil?
             @logger.warn("Dead package: #{line.strip}")
             return
         end
 
-        if (result_set = Atom.get_ebuilds(result)).nil?
+        if (result_set = Atom.get_ebuilds(result)).size == 0
             @logger.warn("Dead PV: #{line.strip}")
             return
         end
@@ -55,8 +53,8 @@ klass = Class.new(Tasks::Runner) do
         result_set.each { |ebuild_id|
             send_data4insert({'data' => [
                 ebuild_id,
-                arch,
-                keyword,
+                shared_data('arch@id', arch),
+                shared_data('keyword@id', result2['keyword']),
                 shared_data('source@id', self.class::SOURCE)
             ]})
         }
@@ -64,4 +62,3 @@ klass = Class.new(Tasks::Runner) do
 end
 
 Tasks.create_task(__FILE__, klass)
-
